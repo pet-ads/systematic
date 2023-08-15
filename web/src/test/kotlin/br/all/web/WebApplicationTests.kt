e7dcac14900bbd8f03bde3596554429becddd2c8
lucas-ifsp <lucas.ruas@gmail.com>
2023-08-15 00:55:45 -0300

    feat: add web module

---
+++ web/src/test/kotlin/br/all/web/WebApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.all.web
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
