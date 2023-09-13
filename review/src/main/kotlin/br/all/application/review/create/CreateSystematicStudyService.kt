02f456eb20a4cb00a6464e676da27bc7a07088c0
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-09-13 10:42:47 -0300

    feat: implemented CreateSystematicStudyService

---
+++ review/src/main/kotlin/br/all/application/review/create/CreateSystematicStudyService.kt
@@ -0,0 +1,26 @@
+package br.all.application.review.create
+
+import br.all.application.review.repository.SystematicStudyDto
+import br.all.application.review.repository.SystematicStudyRepository
+import br.all.application.review.repository.fromRequestModel
+import br.all.application.review.repository.toDto
+import br.all.application.shared.CouldNotCreateEntityException
+import br.all.domain.model.review.SystematicStudy
+import br.all.domain.services.UuidGeneratorService
+
+class CreateSystematicStudyService(
+    private val systematicStudyRepository : SystematicStudyRepository,
+    private val uuidGeneratorService : UuidGeneratorService,
+) {
+    fun create(requestModel : SystematicStudyRequestModel) : SystematicStudyDto {
+        val id = uuidGeneratorService.next()
+        val systematicStudy = SystematicStudy.fromRequestModel(id, requestModel)
+
+        systematicStudyRepository.create(systematicStudy.toDto())
+
+        return systematicStudyRepository.findById(id)
+                        .orElseThrow { CouldNotCreateEntityException("Could not create a systematic study with " +
+                                "such data!")
+                        }
+    }
+}
---
+++ review/src/main/kotlin/br/all/application/shared/CouldNotCreateEntityException.kt
@@ -0,0 +1,4 @@
+package br.all.application.shared
+
+class CouldNotCreateEntityException(message: String?) : RuntimeException(message) {
+}
