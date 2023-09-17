cad5b7fbf20340b1b4b6ce71c173916c6681628e
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-09-17 19:00:47 -0300

    feat: CreateSystematicStudyService now throws IllegalArgumentException in nonexistent researchers cases

--- review/src/main/kotlin/br/all/application/review/create/CreateSystematicStudyService.kt
+++ review/src/main/kotlin/br/all/application/review/create/CreateSystematicStudyService.kt
@@ -8,6 +8,7 @@ import br.all.application.review.repository.toDto
 import br.all.application.shared.CouldNotCreateEntityException
 import br.all.domain.model.review.SystematicStudy
 import br.all.domain.services.UuidGeneratorService
+import br.all.domain.shared.ddd.Notification
 
 class CreateSystematicStudyService(
     private val systematicStudyRepository : SystematicStudyRepository,
@@ -15,13 +16,20 @@ class CreateSystematicStudyService(
     private val uuidGeneratorService : UuidGeneratorService,
 ) {
     fun create(requestModel : SystematicStudyRequestModel) : SystematicStudyDto {
+        val nonexistentResearchersNotification = Notification()
+
+        requestModel.researchers
+            .filter { !researcherRepository.existsById(it) }
+            .forEach { nonexistentResearchersNotification.addError("There is no researcher with id: $it!") }
+
+        require(nonexistentResearchersNotification.hasNoErrors()) { nonexistentResearchersNotification.message() }
+
         val id = uuidGeneratorService.next()
         val systematicStudy = SystematicStudy.fromRequestModel(id, requestModel)
         systematicStudyRepository.create(systematicStudy.toDto())
 
         return systematicStudyRepository.findById(id)
                         .orElseThrow { CouldNotCreateEntityException("Could not create a systematic study with " +
-                                "such data!")
-                        }
+                                "such data!") }
     }
 }
