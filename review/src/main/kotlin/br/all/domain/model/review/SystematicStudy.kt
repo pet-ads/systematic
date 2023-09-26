34178d635331bed26ee208467d66705513576204
lucas-ifsp <lucas.ruas@gmail.com>
2023-09-26 10:29:30 -0300

    feat (systematic): add the figure and owner and change reviewers to collaborators

--- review/src/main/kotlin/br/all/domain/model/review/SystematicStudy.kt
+++ review/src/main/kotlin/br/all/domain/model/review/SystematicStudy.kt
@@ -3,29 +3,24 @@ package br.all.domain.model.review
 import br.all.domain.model.researcher.ResearcherId
 import br.all.domain.shared.ddd.Entity
 
-// todo: replace type title and description properties from String to Text when it is implemented
 class SystematicStudy(
     val id : ReviewId,
     val title : String,
     val description : String,
-    val researchers : MutableSet<ResearcherId>,
+    var owner : ResearcherId,
+    val collaborators : MutableSet<ResearcherId>,
 ) : Entity(id) {
-    init {
-        require(researchers.isNotEmpty()) { "There must be at least one researcher working on this systematic study!" }
-    }
 
     companion object
 
-    fun addResearcher(researcherId : ResearcherId) = researchers.add(researcherId)
+    fun addResearcher(researcherId : ResearcherId) = collaborators.add(researcherId)
 
     fun removeResearcher(researcherId: ResearcherId) {
-        if (researchers.size == 1)
-            throw IllegalStateException("Unable to remove the last researcher! There must be at least one researcher" +
-                    " working on this systematic study!")
-        researchers.remove(researcherId)
+        require(researcherId != owner) {"Can not remove the Systematic Study owner: $owner"}
+        collaborators.remove(researcherId)
     }
 
     override fun toString(): String {
-        return "SystematicStudy(id=$id, title='$title', description='$description', researchers=$researchers)"
+        return "SystematicStudy(id=$id, title='$title', description='$description', researchers=$collaborators)"
     }
 }
