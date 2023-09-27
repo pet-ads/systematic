da35159db165ee3ce120c630dc08d0f3c19b2161
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-09-27 20:14:19 -0300

    feat(application): create service to change selection status

---
+++ review/src/main/kotlin/br/all/application/study/change/ChangeStudyReviewSelectionStatus.kt
@@ -0,0 +1,20 @@
+package br.all.application.study.change
+
+import br.all.application.study.create.StudyReviewRequestModel
+import br.all.application.study.repository.StudyReviewRepository
+import br.all.application.study.repository.fromStudyRequestModel
+import br.all.domain.model.review.ReviewId
+import br.all.domain.model.study.SelectionStatus
+import br.all.domain.model.study.StudyReview
+import br.all.domain.model.study.StudyReviewId
+import java.util.UUID
+
+class ChangeStudyReviewSelectionStatus(
+    private val repository: StudyReviewRepository
+) {
+    fun changeSelectionStatus(reviewID: ReviewId, studyReviewId: StudyReviewId, newSelectionStatus: SelectionStatus){
+        //Encontrar StudyReview
+        val studyReviewDTO = repository.findById(reviewID.value, studyReviewId.value)
+
+    }
+}
