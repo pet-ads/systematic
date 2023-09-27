87ef4d562691d00903c29e980781d8ba4dbf1767
Otávio Pereira Lopes <otavio.pereira.lopes@gmail.com>
2023-09-27 15:27:56 -0300

    Feat: Implements SearchSessionAGRE and SearchSessionID.

---
+++ review/src/main/kotlin/br/all/domain/model/session/SearchSessionID.kt
@@ -0,0 +1,19 @@
+package br.all.domain.model.session
+
+import br.all.domain.shared.ddd.Identifier
+import br.all.domain.shared.ddd.Notification
+import java.util.UUID
+
+@JvmInline
+value class SearchSessionID(val value: UUID) : Identifier {
+    init {
+        val notification = validate()
+        require(notification.hasNoErrors()) { notification.message() }
+    }
+
+    override fun validate(): Notification = Notification()
+
+    override fun toString(): String {
+        return value.toString()
+    }
+}
---
+++ review/src/main/kotlin/br/all/domain/model/session/session.kt
@@ -0,0 +1,5 @@
+package br.all.domain.model.session
+
+class Session {
+
+}
