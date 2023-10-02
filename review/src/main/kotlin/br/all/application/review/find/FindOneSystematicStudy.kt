74f9eb2d44901aaf87748341d6c220726dcdc6c4
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-02 19:39:52 -0300

    feat: FindOneSystematicStudyService now returns an empty response model when there is not such study

--- review/src/main/kotlin/br/all/application/review/find/FindOneSystematicStudy.kt
+++ review/src/main/kotlin/br/all/application/review/find/FindOneSystematicStudy.kt
@@ -1,13 +1,18 @@
 package br.all.application.review.find
 
+import br.all.application.review.repository.SystematicStudyDto
 import br.all.application.review.repository.SystematicStudyRepository
 import java.util.*
 
 class FindOneSystematicStudy(private val systematicStudyRepository: SystematicStudyRepository) {
     fun findById(studyId: UUID) : SystematicStudyResponseModel {
-        val foundStudy = systematicStudyRepository.findById(studyId) ?:
-                            throw NoSuchElementException("Could not find a systematic study with id: $studyId")
-        return SystematicStudyResponseModel(listOf(foundStudy))
+        val studies = mutableListOf<SystematicStudyDto>()
+
+        systematicStudyRepository.findById(studyId)?.let{
+            studies.add(it)
+        }
+
+        return SystematicStudyResponseModel(studies)
     }
 
     fun existById(studyId: UUID) = systematicStudyRepository.findById(studyId) != null
