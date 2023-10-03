6c5ba5b6c536d5254048883f19617fee02667a36
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-03 13:31:36 -0300

    feat: added an overrife of the utility function requireThatExists() that receives a lambda function

--- review/src/main/kotlin/br/all/application/shared/utils/Utilities.kt
+++ review/src/main/kotlin/br/all/application/shared/utils/Utilities.kt
@@ -3,3 +3,6 @@ package br.all.application.shared.utils
 fun requireThatExists(doesItExist: Boolean, message: () -> String = { "There is no such element!" }) {
     if (!doesItExist) throw NoSuchElementException(message())
 }
+
+fun <T> requireThatExists(value: () -> T?, message: () -> String = { "There is not such element" }) : T =
+    value() ?: throw NoSuchElementException(message())
