910d61418afe57204e68dee6dc487924c513498f
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-03 09:29:58 -0300

    test: the owner of an systematic study should be successfully changed

---
+++ review/src/test/kotlin/br/all/application/review/update/ChangeSystematicStudyOwnerServiceTest.kt
@@ -0,0 +1,46 @@
+package br.all.application.review.update
+
+import br.all.application.researcher.repository.ResearcherRepository
+import br.all.application.review.repository.SystematicStudyRepository
+import br.all.application.review.repository.toDto
+import br.all.domain.model.researcher.ResearcherId
+import br.all.domain.model.review.ReviewId
+import br.all.domain.model.review.SystematicStudy
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.util.*
+import kotlin.test.assertEquals
+
+@ExtendWith(MockKExtension::class)
+class ChangeSystematicStudyOwnerServiceTest {
+    @MockK
+    private lateinit var systematicStudyRepository : SystematicStudyRepository
+    @MockK
+    private lateinit var researcherRepository : ResearcherRepository
+    private lateinit var sut : ChangeSystematicStudyOwnerService
+
+    @BeforeEach
+    fun setUp() {
+        sut = ChangeSystematicStudyOwnerService(systematicStudyRepository, researcherRepository)
+    }
+
+    @Test
+    fun `Should the owner be successfully changed`() {
+        val reviewId = UUID.randomUUID()
+        val newOwnerId = UUID.randomUUID()
+        val systematicStudy = SystematicStudy(ReviewId(reviewId), "Some title", "Some description",
+            ResearcherId(UUID.randomUUID()))
+
+        every { researcherRepository.existsById(newOwnerId) } returns true
+        every { systematicStudyRepository.findById(reviewId) } returns systematicStudy.toDto()
+        systematicStudy.changeOwner(ResearcherId(newOwnerId))
+        every { systematicStudyRepository.create(systematicStudy.toDto()) } returns Unit
+
+        val updatedStudy = sut.changeOwner(reviewId, newOwnerId)
+        assertEquals(newOwnerId, updatedStudy.owner)
+    }
+}
