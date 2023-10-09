fa6c5f9b2887f6e17f884dffc301da6919f12f80
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-10-09 17:27:19 -0300

    chore(application): remove dev comments

--- review/src/main/kotlin/br/all/application/study/update/UpdateStudyReviewSelectionService.kt
+++ review/src/main/kotlin/br/all/application/study/update/UpdateStudyReviewSelectionService.kt
@@ -8,13 +8,12 @@ import br.all.domain.model.study.StudyReview
 class UpdateStudyReviewSelectionService(private val repository: StudyReviewRepository) {
     //TODO always create request model to wrap the request data
     fun changeStatus(requestModel: UpdateStudyReviewRequestModel){
-        //Encontrar StudyReview
         val studyReviewDto = repository.findById(requestModel.reviewID, requestModel.studyReviewId)
-        //Trasformar DTO em objeto
+
         //TODO I solved the map to modifiableMap issue
         val studyReview = StudyReview.fromDto(studyReviewDto)
 
-        //Mudar Selection Status //TODO Remove all comments explaining the code
+        //TODO Remove all comments explaining the code
         when(requestModel.newStatus.uppercase()){
             "UNCLASSIFIED" -> studyReview.unclassifyInSelection()
             "DUPLICATED" -> studyReview.markAsDuplicate()
@@ -23,7 +22,6 @@ class UpdateStudyReviewSelectionService(private val repository: StudyReviewRepos
             else -> throw IllegalArgumentException("Unknown study review status: ${requestModel.newStatus}.")
         }
 
-        //Salvar no banco
         repository.create(studyReview.toDto())
     }
 }
