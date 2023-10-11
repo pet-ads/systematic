f39b2bb3bb084be4b86248d19768ca8852ea6129
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-11 08:48:42 -0300

    feat: added .add() .remove() methods for protocol sets

--- review/src/main/kotlin/br/all/domain/model/protocol/Protocol.kt
+++ review/src/main/kotlin/br/all/domain/model/protocol/Protocol.kt
@@ -41,4 +41,37 @@ class Protocol(
 
     private val _qualityFormFields = qualityFormFields.toMutableSet()
     val qualityFormFields get() = _qualityFormFields.toSet()
+
+    fun addKeyword(keyword: String) = _keywords.add(keyword)
+
+    fun removeKeyword(keyword: String) = _keywords.removeIfSizeGreaterThanOne(keyword)
+                                            { "There must be at least one keyword defined!" }
+
+    fun addSearchSource(searchSource: SearchSource) = _sources.add(searchSource)
+
+    fun removeSearchSource(searchSource: SearchSource) = _sources.removeIfSizeGreaterThanOne(searchSource)
+                                                { "There must be at least one search source!" }
+
+    fun addLanguage(language: Language) = _studiesLanguages.add(language)
+
+    fun removeLanguage(language: Language) = _studiesLanguages.removeIfSizeGreaterThanOne(language)
+                                                        { "At least one language must be specified!" }
+
+    fun addSelectionCriteria(criteria: Criteria) = _selectionCriteria.add(criteria)
+
+    fun removeSelectionCriteria(criteria: Criteria) = _selectionCriteria.removeIfSizeGreaterThanOne(criteria)
+                                                        { "The protocol must specify the study selection criteria" }
+
+    fun addExtractionField(questionId: QuestionId) = _extractionFormFields.add(questionId)
+
+    fun removeExtractionField(questionId: QuestionId) = _extractionFormFields.removeIfSizeGreaterThanOne(questionId)
+                                    { "There must be specified at least one question for the extraction form!" }
+
+    fun addQualityField(questionId: QuestionId) = _qualityFormFields.add(questionId)
+
+    fun removeQualityField(questionId: QuestionId) = _qualityFormFields.removeIfSizeGreaterThanOne(questionId)
+                                    { "There must be specified at least one question for the quality form!" }
+
+    private inline fun <T> MutableSet<T>.removeIfSizeGreaterThanOne(element: T, message: () -> String) =
+        if (size > 1) remove(element) else throw IllegalStateException(message())
 }
