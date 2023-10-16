40da046944e57009b3a0bbbbe89ba362b988304f
Otávio Pereira Lopes <otavio.pereira.lopes@gmail.com>
2023-10-16 17:47:12 -0300

    Almost finish cerateSearchSession and doing tests

--- review/src/main/kotlin/br/all/application/search/create/CreateSearchSessionService.kt
+++ review/src/main/kotlin/br/all/application/search/create/CreateSearchSessionService.kt
@@ -1,6 +1,5 @@
-package br.all.application.search.create
-
 import br.all.application.review.repository.SystematicStudyRepository
+import br.all.application.search.create.SearchSessionRequestModel
 import br.all.application.search.find.SearchSessionResponseModel
 import br.all.application.search.repository.SearchSessionRepository
 import br.all.domain.model.search.SearchSession
@@ -10,12 +9,15 @@ import br.all.domain.services.UuidGeneratorService
 class CreateSearchSessionService(
     private val systematicStudyRepository: SystematicStudyRepository,
     private val searchSessionRepository: SearchSessionRepository,
-    private val uuidGeneratorService: UuidGeneratorService
-) {
+    private val uuidGeneratorService: UuidGeneratorService)
+{
     fun createSession(requestModel: SearchSessionRequestModel): SearchSessionResponseModel {
         require(requestModel.searchString.isNotBlank()) { "Search string must not be blank" }
+
+        //poderia existir uma funcao para isso->isValid talvez?
+        //val isValidSystematicStudy = systematicStudyRepository.isValidSystematicStudy(requestModel.systematicStudyId)
+       // require(isValidSystematicStudy) { "Invalid systematic study ID" }
         val existingSession = searchSessionRepository.getSessionBySource(requestModel.source)
-        //implements a better logic
         require(existingSession == null) { "A session for this SearchSource already exists" }
 
         val sessionId = SearchSessionID(uuidGeneratorService.next())
@@ -24,11 +26,9 @@ class CreateSearchSessionService(
             sessionId,
             requestModel.searchString,
             requestModel.additionalInfo ?: "",
-            source = requestModel.source
+            source = requestModel.source,
+           // systematicStudyId = requestModel.systematicStudyId
         )
-
         searchSessionRepository.create(searchSession)
-
         return SearchSessionResponseModel(sessionId.toString(), "Search session created successfully.")
-    }
-}
+    } }
---
+++ review/src/test/kotlin/br/all/domain/model/search/SearchSessionTest.kt
@@ -0,0 +1,32 @@
+package br.all.domain.model.search
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class SearchSessionTest {
+
+    @Test
+    fun testToString() {
+    }
+
+    @Test
+    fun getId() {
+    }
+
+    @Test
+    fun getSearchString() {
+    }
+
+    @Test
+    fun getAdditionalInfo() {
+    }
+
+    @Test
+    fun getTimestamp() {
+    }
+
+    @Test
+    fun getSource() {
+    }
+}
