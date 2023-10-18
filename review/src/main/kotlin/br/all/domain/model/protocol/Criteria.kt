90c20eaace4b7e8a286df23e4897287bc8a1cce9
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-18 10:22:06 -0300

    feat: added CriteriaType and a init block Criteria

--- review/src/main/kotlin/br/all/domain/model/protocol/Criteria.kt
+++ review/src/main/kotlin/br/all/domain/model/protocol/Criteria.kt
@@ -3,7 +3,17 @@ package br.all.domain.model.protocol
 import br.all.domain.shared.ddd.Notification
 import br.all.domain.shared.ddd.ValueObject
 
-data class Criteria(val description: String) : ValueObject() {
+data class Criteria(
+    val description: String,
+    val type: CriteriaType,
+) : ValueObject() {
+    enum class CriteriaType { INCLUSION, EXCLUSION }
+
+    init {
+        val notification = validate()
+        require(notification.hasNoErrors()) { notification.message() }
+    }
+
     override fun validate(): Notification {
         TODO("Not yet implemented")
     }
