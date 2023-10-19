3153ac96654eebd5ba8d1d33d4288a07945a68bf
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-19 20:47:24 -0300

    test: phrase with words of uppercase letters should be accepted as well

--- review/src/test/kotlin/br/all/domain/model/protocol/CriteriaTest.kt
+++ review/src/test/kotlin/br/all/domain/model/protocol/CriteriaTest.kt
@@ -8,4 +8,11 @@ class CriteriaTest {
     fun `Should accept words with only lowercase letters`() {
         assertDoesNotThrow { Criteria("phrase with only lower case words", Criteria.CriteriaType.INCLUSION) }
     }
+
+    @Test
+    fun `Should accept words with only uppercase letters`() {
+        assertDoesNotThrow {
+            Criteria("PHRASE WITH WORDS OF ONLY UPPERCASE LETTERS", Criteria.CriteriaType.INCLUSION)
+        }
+    }
 }
