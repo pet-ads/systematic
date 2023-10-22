dfbc9cbe594057cb52cfefd45e82f2cf59dd70a9
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-22 17:05:11 -0300

    test: fixed broken tests in UpdateSystematicStudyServiceTest

--- review/src/test/kotlin/br/all/application/review/update/UpdateSystematicStudyServiceTest.kt
+++ review/src/test/kotlin/br/all/application/review/update/UpdateSystematicStudyServiceTest.kt
@@ -3,24 +3,19 @@ package br.all.application.review.update
 import br.all.application.review.repository.SystematicStudyDto
 import br.all.application.review.repository.SystematicStudyRepository
 import br.all.application.review.repository.toDto
+import br.all.application.review.util.FakeSystematicStudyRepository
 import br.all.domain.model.researcher.ResearcherId
 import br.all.domain.model.review.ReviewId
 import br.all.domain.model.review.SystematicStudy
-import io.mockk.every
-import io.mockk.impl.annotations.MockK
-import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import java.util.*
-import kotlin.test.assertEquals
 
-@ExtendWith(MockKExtension::class)
 class UpdateSystematicStudyServiceTest{
-    @MockK
     private lateinit var systematicStudyRepository: SystematicStudyRepository
     private lateinit var systematicStudyId: UUID
     private lateinit var updatingSystematicStudy: SystematicStudy
@@ -29,8 +24,8 @@ class UpdateSystematicStudyServiceTest{
 
     @BeforeEach
     fun setUp() {
+        systematicStudyRepository = FakeSystematicStudyRepository()
         sut = UpdateSystematicStudyService(systematicStudyRepository)
-
         systematicStudyId = UUID.randomUUID()
         updatingSystematicStudy = SystematicStudy(ReviewId(systematicStudyId), "Old title",
                             "Old description", ResearcherId(UUID.randomUUID()))
@@ -41,40 +36,46 @@ class UpdateSystematicStudyServiceTest{
     fun `Should only the title be updated`() {
         val requestModel = UpdateSystematicStudyRequestModel("New title", null)
 
-        updatingSystematicStudy.rename("New title")
+        updatingSystematicStudy.title = "New title"
         val newDto = updatingSystematicStudy.toDto()
-        mockkRepositoryToSunnyDay(newDto)
+        systematicStudyRepository.create(oldDto)
 
-        val updateStudy = sut.update(systematicStudyId, requestModel)
-        assertEquals("New title", updateStudy.title)
-        assertEquals("Old description", updateStudy.description)
+        sut.update(systematicStudyId, requestModel)
+        val updateStudy = systematicStudyRepository.findById(systematicStudyId)
+
+        assertEquals("New title", updateStudy?.title)
+        assertEquals("Old description", updateStudy?.description)
     }
 
     @Test
     fun `Should only the description be updated`() {
         val requestModel = UpdateSystematicStudyRequestModel(null, "New description")
 
-        updatingSystematicStudy.changeDescription("New description")
+        updatingSystematicStudy.description = "New description"
         val newDto = updatingSystematicStudy.toDto()
-        mockkRepositoryToSunnyDay(newDto)
+        systematicStudyRepository.create(oldDto)
 
-        val updatedStudy = sut.update(systematicStudyId, requestModel)
-        assertEquals("Old title", updatedStudy.title)
-        assertEquals("New description", updatedStudy.description)
+        sut.update(systematicStudyId, requestModel)
+        val updatedStudy = systematicStudyRepository.findById(systematicStudyId)
+
+        assertEquals("Old title", updatedStudy?.title)
+        assertEquals("New description", updatedStudy?.description)
     }
 
     @Test
     fun `Should both title and description be updated`() {
         val requestModel = UpdateSystematicStudyRequestModel("New title", "New description")
 
-        updatingSystematicStudy.rename("New title")
-        updatingSystematicStudy.changeDescription("New description")
+        updatingSystematicStudy.title = "New title"
+        updatingSystematicStudy.description = "New description"
         val newDto = updatingSystematicStudy.toDto()
-        mockkRepositoryToSunnyDay(newDto)
+        systematicStudyRepository.create(oldDto)
 
-        val updatedStudy = sut.update(systematicStudyId, requestModel)
-        assertEquals("New title", updatedStudy.title)
-        assertEquals("New description", updatedStudy.description)
+        sut.update(systematicStudyId, requestModel)
+        val updatedStudy = systematicStudyRepository.findById(systematicStudyId)
+
+        assertEquals("New title", updatedStudy?.title)
+        assertEquals("New description", updatedStudy?.description)
     }
 
     @ParameterizedTest
@@ -82,25 +83,21 @@ class UpdateSystematicStudyServiceTest{
     fun `Should the study keep the same`(title: String?, description: String?) {
         val requestModel = UpdateSystematicStudyRequestModel(title, description)
 
-        updatingSystematicStudy.rename(title ?: updatingSystematicStudy.title)
-        updatingSystematicStudy.changeDescription(description ?: updatingSystematicStudy.description)
+        updatingSystematicStudy.title = title ?: updatingSystematicStudy.title
+        updatingSystematicStudy.description = description ?: updatingSystematicStudy.description
         val newDto = updatingSystematicStudy.toDto()
-        mockkRepositoryToSunnyDay(newDto)
+        systematicStudyRepository.create(oldDto)
 
-        val updatedStudy = sut.update(systematicStudyId, requestModel)
-        assertEquals("Old title", updatedStudy.title)
-        assertEquals("Old description", updatedStudy.description)
+        sut.update(systematicStudyId, requestModel)
+        val updatedStudy = systematicStudyRepository.findById(systematicStudyId)
+
+        assertEquals("Old title", updatedStudy?.title)
+        assertEquals("Old description", updatedStudy?.description)
     }
 
     @Test
     fun `Should throw NoSuchElementException for nonexistent systematic study`() {
         val requestModel = UpdateSystematicStudyRequestModel(null, null)
-        every { systematicStudyRepository.findById(systematicStudyId) } returns null
         assertThrows<NoSuchElementException> { sut.update(systematicStudyId, requestModel) }
-    }
-
-    private fun mockkRepositoryToSunnyDay(newDto: SystematicStudyDto) {
-        every { systematicStudyRepository.findById(systematicStudyId) } returns oldDto
-        every { systematicStudyRepository.create(newDto) } returns Unit
     }
 }
