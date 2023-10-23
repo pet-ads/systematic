f371247ddd68e52a43dbeb3e298ab3b4ef1fc255
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-23 09:48:38 -0300

    feat: implemented PICOC using Phrases instead of String

--- review/src/main/kotlin/br/all/domain/model/protocol/PICOC.kt
+++ review/src/main/kotlin/br/all/domain/model/protocol/PICOC.kt
@@ -2,15 +2,14 @@ package br.all.domain.model.protocol
 
 import br.all.domain.shared.ddd.Notification
 import br.all.domain.shared.ddd.ValueObject
+import br.all.domain.shared.utils.phrase.Phrase
 
 data class PICOC(
-    val population: String,
-    val intervention: String,
-    val control: String,
-    val outcome: String,
-    val context: String,
+    val population: Phrase,
+    val intervention: Phrase,
+    val control: Phrase,
+    val outcome: Phrase,
+    val context: Phrase?,
 ) : ValueObject() {
-    override fun validate(): Notification {
-        TODO("Not yet implemented")
-    }
+    override fun validate() = Notification()
 }
