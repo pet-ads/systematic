dc50e2f37b3b3cbf2ed2613fcb7024ee3a868bb3
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-24 08:54:07 -0300

    feat: implemented ResearchQuestion using Phrase value object

--- review/src/main/kotlin/br/all/domain/model/protocol/ResearchQuestion.kt
+++ review/src/main/kotlin/br/all/domain/model/protocol/ResearchQuestion.kt
@@ -2,9 +2,8 @@ package br.all.domain.model.protocol
 
 import br.all.domain.shared.ddd.Notification
 import br.all.domain.shared.ddd.ValueObject
+import br.all.domain.shared.utils.phrase.Phrase
 
-data class ResearchQuestion(val description: String) : ValueObject() {
-    override fun validate(): Notification {
-        TODO("Not yet implemented")
-    }
+data class ResearchQuestion(val description: Phrase) : ValueObject() {
+    override fun validate() = Notification()
 }
