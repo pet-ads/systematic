1fd02860f66a7c87092b860a468a3ad773ae66af
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-25 11:36:29 -0300

    feat: added a validating init block to Protocol

--- review/src/main/kotlin/br/all/domain/model/protocol/Protocol.kt
+++ review/src/main/kotlin/br/all/domain/model/protocol/Protocol.kt
@@ -2,6 +2,7 @@ package br.all.domain.model.protocol
 
 import br.all.domain.model.review.ReviewId
 import br.all.domain.shared.ddd.Entity
+import br.all.domain.shared.ddd.Notification
 import br.all.domain.shared.utils.Language
 import br.all.domain.shared.utils.phrase.Phrase
 
@@ -74,6 +75,32 @@ class Protocol(
     fun removeQualityField(questionId: QuestionId) = _qualityFormFields.removeKeepingOne(questionId)
                                     { "There must be specified at least one question for the quality form!" }
 
+    init {
+        val notification = validate()
+        require(notification.hasNoErrors()) { notification.message() }
+    }
+
     private inline fun <T> MutableSet<T>.removeKeepingOne(element: T, message: () -> String) =
         if (size > 1) remove(element) else throw IllegalStateException(message())
+
+    fun validate(): Notification {
+        val notification = Notification()
+
+        if (searchString.isBlank())
+            notification.addError("The search string cannot be blank!")
+        if (researchQuestions.isEmpty())
+            notification.addError("The protocol must specify at least one research question!")
+        if (_keywords.isEmpty() || _keywords.any { it.isBlank() })
+            notification.addError("There must be at least one keyword and none of them can be blank!")
+        if (_informationSources.isEmpty())
+            notification.addError("The protocol must specify the information sources for its systematic study!")
+        if (_studiesLanguages.isEmpty())
+            notification.addError("The protocol must specify the studies languages!")
+        if (_selectionCriteria.none { it.type == Criteria.CriteriaType.INCLUSION })
+            notification.addError("At least one studies inclusion criterion must be given!")
+        if (_selectionCriteria.none { it.type == Criteria.CriteriaType.EXCLUSION })
+            notification.addError("At least one studies exclusion criterion must be given!")
+
+        return notification
+    }
 }
