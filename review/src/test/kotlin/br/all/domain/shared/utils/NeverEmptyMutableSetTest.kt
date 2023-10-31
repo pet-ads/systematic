8bdb6300f09fe5853d50409f0adf5e6c9edd876e
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-31 13:53:35 -0300

    test: the remaining elemented when clearing an NeverEmptyMutableSet should be the one passed as argument

--- review/src/test/kotlin/br/all/domain/shared/utils/NeverEmptyMutableSetTest.kt
+++ review/src/test/kotlin/br/all/domain/shared/utils/NeverEmptyMutableSetTest.kt
@@ -1,6 +1,7 @@
 package br.all.domain.shared.utils
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 
@@ -15,5 +16,15 @@ class NeverEmptyMutableSetTest {
         val sut = neverEmptyMutableSetOf(10, 25, 30, 88)
         sut.clear()
         assertEquals(1, sut.size)
+    }
+    
+    @Test
+    fun `Should keep the correct element when clearing an NeverEmptyMutableSet`() {
+        val sut = neverEmptyMutableSetOf(10, 25, 30, 88)
+        sut.clear(30)
+        assertAll(
+            { assertEquals(1, sut.size) },
+            { assertEquals(30, sut.first()) }
+        )
     }
 }
