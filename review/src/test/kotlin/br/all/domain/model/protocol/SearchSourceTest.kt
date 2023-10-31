2011060829ec4bb27c46d2f91b01ef8b6e686723
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-30 23:57:07 -0300

    test: valid search source creation

--- review/src/test/kotlin/br/all/domain/model/protocol/SearchSourceTest.kt
+++ review/src/test/kotlin/br/all/domain/model/protocol/SearchSourceTest.kt
@@ -1,5 +1,11 @@
 package br.all.domain.model.protocol
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+
 internal class SearchSourceTest {
-    
+    @Test
+    fun `Should successfully create a valid search source`() {
+        assertDoesNotThrow { SearchSource("Valid SearchSource") }
+    }
 }
