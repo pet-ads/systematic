8b96853194f4fa0ae34c41270474c92c9abadaf0
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-31 13:36:53 -0300

    test: a one element should remain when clearing a NeverEmptyMutableSet

--- review/src/test/kotlin/br/all/domain/shared/utils/NeverEmptyMutableSetTest.kt
+++ review/src/test/kotlin/br/all/domain/shared/utils/NeverEmptyMutableSetTest.kt
@@ -2,10 +2,18 @@ package br.all.domain.shared.utils
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
 
 class NeverEmptyMutableSetTest {
     @Test
     fun `Should throw if attempting to create an NeverEmptyMutableSet from an empty source`() {
         assertThrows<IllegalArgumentException> { NeverEmptyMutableSet<Int>() }
+    }
+    
+    @Test
+    fun `Should keep at least one elements when clearing an NeverEmptyMutableSet`() {
+        val sut = neverEmptyMutableSetOf(10, 25, 30, 88)
+        sut.clear()
+        assertEquals(1, sut.size)
     }
 }
