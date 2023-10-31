0051fb99fba7eaa87232113fcbfeef840965a463
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-31 00:18:32 -0300

    test: a search source should contain digits or symbols

--- review/src/test/kotlin/br/all/domain/model/protocol/SearchSourceTest.kt
+++ review/src/test/kotlin/br/all/domain/model/protocol/SearchSourceTest.kt
@@ -2,10 +2,16 @@ package br.all.domain.model.protocol
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 
 internal class SearchSourceTest {
     @Test
     fun `Should successfully create a valid search source`() {
         assertDoesNotThrow { SearchSource("Valid SearchSource") }
+    }
+
+    @Test
+    fun `Should throw if search source contains digits or symbols`() {
+        assertThrows<IllegalArgumentException> { SearchSource("H& @ba O-O Dxd5 1m J0ta") }
     }
 }
