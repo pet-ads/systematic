9cd06c0052a82330a561b4a364e91a107a408e2e
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-31 13:48:52 -0300

    refactor: added an explicit delegate to NeverEmptyMutableSet

--- review/src/main/kotlin/br/all/domain/shared/utils/NeverEmptyMutableSet.kt
+++ review/src/main/kotlin/br/all/domain/shared/utils/NeverEmptyMutableSet.kt
@@ -2,9 +2,12 @@ package br.all.domain.shared.utils
 
 import java.util.function.Predicate
 
-class NeverEmptyMutableSet<T>(
-    vararg source: T
-): MutableSet<T> by mutableSetOf() {
+class NeverEmptyMutableSet<T> private constructor(
+    private val innerSet: MutableSet<T>,
+    vararg source: T,
+): MutableSet<T> by innerSet {
+    constructor(vararg source: T) : this(mutableSetOf(), source = source)
+
     init {
         require(source.isNotEmpty()) { "Unable to create a NeverEmptyMutableSet from an empty source of elements!" }
         addAll(source)
@@ -15,35 +18,35 @@ class NeverEmptyMutableSet<T>(
     override fun clear() = clear(elementAt(0))
 
     fun clear(keepingElement: T) {
-        removeIf{ it != keepingElement }
+        innerSet.removeIf { it == keepingElement }
     }
 
     override fun remove(element: T): Boolean {
         check(size > 1) { "Unable to remove elements so far because it would cause this set to be empty!" }
-        return removeIf{ it == element }
+        return innerSet.remove(element)
     }
 
     override fun removeAll(elements: Collection<T>): Boolean {
         require(elements.size < size || elements.size == size && !containsAll(elements))
             { "Unable to remove elements: $elements; because it would cause this set to be empty!" }
-        return removeIf{ it in elements }
+        return innerSet.removeAll(elements.toSet())
     }
 
     override fun retainAll(elements: Collection<T>): Boolean {
-        require(elements.isEmpty() || elements.none{ it in this }) {
+        require(elements.isEmpty() || elements.none{ it in innerSet }) {
             "At least one element should be kept in this set! The provided collection: $elements; would cause " +
                     "in all elements being removed!"
         }
-        return removeIf{ it in elements }
+        return innerSet.retainAll(elements.toSet())
     }
 
     override fun removeIf(filter: Predicate<in T>): Boolean {
-        return super.removeIf(filter.and { size > 1 })
+        return innerSet.removeIf(filter.and { size > 1 })
     }
 
     operator fun get(index: Int) = elementAt(index)
 
-    fun toMutableSet() = mutableSetOf(this)
+    fun toMutableSet() = innerSet.toMutableSet()
 }
 
 fun <T> Set<T>.toNeverEmptyMutableSet() = NeverEmptyMutableSet(elementAt(0)).also { it.addAll(this) }
