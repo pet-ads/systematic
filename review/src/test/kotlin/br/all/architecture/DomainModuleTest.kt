c90b2a7e4a871300de104253cd00c96712385855
lucas-ifsp <lucas.ruas@gmail.com>
2023-11-06 00:35:33 -0300

    test(systematic): add archunit tests to ensure architecture compliance

---
+++ review/src/test/kotlin/br/all/architecture/DomainModuleTest.kt
@@ -0,0 +1,63 @@
+package br.all.architecture
+
+import com.tngtech.archunit.core.domain.JavaClasses
+import com.tngtech.archunit.core.importer.ImportOption
+import com.tngtech.archunit.junit.AnalyzeClasses
+import com.tngtech.archunit.junit.ArchTest
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
+
+
+@AnalyzeClasses(packages = ["br.all"], importOptions = [ImportOption.DoNotIncludeTests::class])
+class DomainModuleTest {
+
+    @ArchTest
+    fun `should domain have no dependencies`(importedClasses: JavaClasses) =
+        noClasses().that()
+            .resideInAPackage("..domain..")
+            .should().dependOnClassesThat().resideOutsideOfPackages("..domain..", "java..", "kotlin..", "org.jetbrains..")
+            .check(importedClasses)
+
+    @ArchTest
+    fun `should domain only be accessed by application or itself`(importedClasses: JavaClasses) =
+        classes().that().resideInAPackage("..domain..")
+            .should().onlyBeAccessed().byAnyPackage("..application..", "..domain..")
+            .check(importedClasses)
+
+    @ArchTest
+    fun `should shared classes in domain have not dependencies outside shared package`(importedClasses: JavaClasses) =
+        noClasses().that().resideInAPackage("br.all.domain.shared..")
+            .should().dependOnClassesThat().resideOutsideOfPackages("br.all.domain.shared..", "java..", "kotlin..", "org.jetbrains..")
+            .check(importedClasses)
+
+    @ArchTest
+    fun `should application not depend on infraestructure`(importedClasses: JavaClasses) =
+        noClasses().that().resideInAPackage("..application..")
+            .should().accessClassesThat().resideInAPackage("..infrastructure..")
+            .check(importedClasses)
+
+    @ArchTest
+    fun `should application only be accessed by infrastructure, service or itself`(importedClasses: JavaClasses) =
+        classes().that().resideInAPackage("..application..")
+            .should().onlyBeAccessed().byAnyPackage("..application..", "..infrastructure..", "..services..")
+            .check(importedClasses)
+
+    @ArchTest
+    fun `should application repositories be abstractions`(importedClasses: JavaClasses) =
+        classes().that()
+            .resideInAPackage("..application..")
+            .and()
+            .haveSimpleNameEndingWith("Repository")
+            .should().beInterfaces()
+            .check(importedClasses)
+
+    @ArchTest
+    fun `should concrete repositories be located inside infrastructure`(importedClasses: JavaClasses) =
+        classes().that()
+            .haveSimpleNameEndingWith("Repository")
+            .and()
+            .resideOutsideOfPackages("..application..")
+            .should().resideInAPackage("..infrastructure..")
+            .check(importedClasses)
+
+}
