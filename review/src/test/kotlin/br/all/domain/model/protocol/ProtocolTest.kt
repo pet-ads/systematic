96e2213e71883aae961630f12230a9afab9461b2
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-06 18:36:05 -0300

    test: a protocol should not be created with no exclusion criteria

--- review/src/test/kotlin/br/all/domain/model/protocol/ProtocolTest.kt
+++ review/src/test/kotlin/br/all/domain/model/protocol/ProtocolTest.kt
@@ -73,4 +73,25 @@ class ProtocolTest {
                 .finish()
         }
     }
+
+    @Test
+    fun `Should throw if there is no exclusion criteria`() {
+        val protocolId = ProtocolId(UUID.randomUUID())
+        val reviewId = ReviewId(UUID.randomUUID())
+        assertThrows<IllegalArgumentException> {
+            Protocol.write().identifiedBy(protocolId, reviewId, setOf("Keyword"))
+                .researchesFor(Phrase("Something")).because(Phrase("It is important"))
+                .toAnswer(setOf(ResearchQuestion(Phrase("What is the question which its answer is 42?"))))
+                .searchProcessWillFollow(Phrase("Reading philosophical articles"), "String")
+                .at(setOf(SearchSource("Some Source With Many Philosophical Articles")))
+                .selectedBecause(Phrase("I want so"))
+                .searchStudiesOf(setOf(Language(Language.LangType.ENGLISH)), Phrase("Primaries and secondaries"))
+                .selectionProcessWillFollowAs(Phrase("Classify articles by criteria"))
+                .selectStudiesBy(setOf(
+                    Criteria(Phrase("It has deep reflexion about life"), Criteria.CriteriaType.INCLUSION),
+                )).collectDataBy(Phrase("Reading the articles and reflect about them"))
+                .analyseDataBy(Phrase("Analyse opinions on each article"))
+                .finish()
+        }
+    }
 }
