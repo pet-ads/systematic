79e2c9af7072337e7560eae243f15da0965cbdaf
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-12 20:55:39 -0300

    refactor: deleted old and unused SystematicStudyResponseModel that were in find packaged - it was renamed

--- review/src/main/kotlin/br/all/application/review/find/SystematicStudyResponseModel.kt
+++
@@ -1,5 +0,0 @@
-package br.all.application.review.find
-
-import br.all.application.review.repository.SystematicStudyDto
-
-data class SystematicStudyResponseModel(val studies: List<SystematicStudyDto>)
