e379818f76b3e564b998803e6639bd0a81a8315e
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-12 11:38:33 -0300

    refactor: created static constructor to Criteria to add more meaning in the creation process

--- review/src/main/kotlin/br/all/domain/model/protocol/Criteria.kt
+++ review/src/main/kotlin/br/all/domain/model/protocol/Criteria.kt
@@ -4,11 +4,17 @@ import br.all.domain.shared.ddd.Notification
 import br.all.domain.shared.ddd.ValueObject
 import br.all.domain.shared.utils.phrase.Phrase
 
-data class Criteria(
+data class Criteria internal constructor(
     val description: Phrase,
     val type: CriteriaType,
 ) : ValueObject() {
     enum class CriteriaType { INCLUSION, EXCLUSION }
+
+    companion object {
+        fun toInclude(description: Phrase) = Criteria(description, CriteriaType.INCLUSION)
+
+        fun toExclude(description: Phrase) = Criteria(description, CriteriaType.EXCLUSION)
+    }
 
     override fun validate() = Notification()
 }
