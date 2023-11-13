df294749fc3ad1ef3df8580119c1c0a506fe47d3
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-12 23:10:10 -0300

    feat: FindOneProtocolService checks if the systematic study exists before searching the protocol

--- review/src/main/kotlin/br/all/application/protocol/find/FindOneProtocolServiceImpl.kt
+++ review/src/main/kotlin/br/all/application/protocol/find/FindOneProtocolServiceImpl.kt
@@ -1,14 +1,28 @@
 package br.all.application.protocol.find
 
+import br.all.application.protocol.repository.ProtocolDto
 import br.all.application.protocol.repository.ProtocolRepository
+import br.all.application.review.repository.SystematicStudyRepository
+import br.all.domain.shared.utils.requireThatExists
 import java.util.*
 
-class FindOneProtocolServiceImpl(val protocolRepository: ProtocolRepository): FindOneProtocolService {
+class FindOneProtocolServiceImpl(
+    val protocolRepository: ProtocolRepository,
+    val systematicStudyRepository: SystematicStudyRepository,
+): FindOneProtocolService {
     override fun findById(id: UUID) = protocolRepository.findById(id)
 
-    override fun findBySystematicStudy(reviewId: UUID) = protocolRepository.findBySystematicStudy(reviewId)
+    override fun findBySystematicStudy(reviewId: UUID): ProtocolDto? {
+        requireThatExists(systematicStudyRepository.existsById(reviewId))
+            { "Cannot find a protocol because there is not a SystematicStudy with id: $reviewId" }
+        return protocolRepository.findBySystematicStudy(reviewId)
+    }
 
     override fun existsById(id: UUID) = protocolRepository.existsById(id)
 
-    override fun existsBySystematicStudy(reviewId: UUID) = protocolRepository.existsBySystematicStudy(reviewId)
+    override fun existsBySystematicStudy(reviewId: UUID): Boolean {
+        requireThatExists(systematicStudyRepository.existsById(reviewId))
+            { "There is not a SystematicStudy with id: $reviewId" }
+        return protocolRepository.existsBySystematicStudy(reviewId)
+    }
 }
