0ece1b16e0c03443bf4760af20f18dc0f493db21
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-12 22:59:34 -0300

    feat: defined methods to find and check if exists by SystematicStudy in ProtocolRepository

--- review/src/main/kotlin/br/all/application/protocol/repository/ProtocolRepository.kt
+++ review/src/main/kotlin/br/all/application/protocol/repository/ProtocolRepository.kt
@@ -7,7 +7,11 @@ interface ProtocolRepository {
 
     fun findById(id: UUID): ProtocolDto?
 
+    fun findBySystematicStudy(systematicStudyId: UUID): ProtocolDto?
+
     fun findAll(): List<ProtocolDto>
 
     fun existsById(id: UUID): Boolean
+
+    fun existsBySystematicStudy(systematicStudyId: UUID): Boolean
 }
