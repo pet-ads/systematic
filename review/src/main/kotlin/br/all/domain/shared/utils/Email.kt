01b915f04e400ad355a4361cbcf41b4dbae1821a
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-14 04:34:20 -0300

    feat: implement size validation and other semantic validations

--- review/src/main/kotlin/br/all/domain/shared/utils/Email.kt
+++ review/src/main/kotlin/br/all/domain/shared/utils/Email.kt
@@ -17,13 +17,38 @@ data class Email(val email: String) : ValueObject() {
         if (email.isBlank()) notification.addError("Email must not be blank.")
         if (!isValidEmailFormat(email)) notification.addError("Wrong Email format.")
 
-        //TODO what is the min and max size of an email? Come on, you can be more psycho finding possible problems than that.
         return notification
     }
 
     private fun isValidEmailFormat(email: String): Boolean {
-        //TODO I have added some failing tests for you. The regex is the right path, but the following is a but simplistic
+        if (email.length > 64) return false
+        if (!validatesSemantics(email)) return false
+        if (hasRepeatedSubdomains(email)) return false
+        if (email.contains("..")) return false
+        if (email.contains("@.")) return false
+        if (email.contains(".@")) return false
+        return true
+    }
+
+    private fun validatesSemantics(email: String): Boolean {
         val regex = Regex("^[A-Za-z0-9+_.-]+@[a-z.]+$")
         return regex.matches(email)
     }
+
+    fun hasRepeatedSubdomains(email: String): Boolean {
+        val parts = email.split("@")
+
+        if (parts.size == 2) {
+            val subdomains = parts[1].split(".")
+            val verifyedSubdomains = HashSet<String>()
+
+            for (subdomain in subdomains){
+                if (!verifyedSubdomains.add(subdomain)) return true
+            }
+        }
+        return false
+    }
+
+
+
 }
