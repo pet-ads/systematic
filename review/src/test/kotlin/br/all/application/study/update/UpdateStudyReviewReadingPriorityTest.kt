f77410d3b6e203f792ac3b1b8ed10411e79c4a0c
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-11-15 05:19:10 -0300

    test: should change reading priority on update

--- review/src/test/kotlin/br/all/application/study/update/UpdateStudyReviewReadingPriorityTest.kt
+++ review/src/test/kotlin/br/all/application/study/update/UpdateStudyReviewReadingPriorityTest.kt
@@ -3,42 +3,38 @@ package br.all.application.study.update
 import br.all.application.repositoryFake.StudyReviewRepositoryFake
 import br.all.application.study.shared.createRequestModel
 import br.all.application.study.shared.createStudyReviewDto
-import io.mockk.junit5.MockKExtension
-import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.Test
 import java.util.*
 
-class UpdateStudyReviewSelectionStatusServiceTest {
-
+class UpdateStudyReviewReadingPriorityTest {
     private lateinit var repository: StudyReviewRepositoryFake
 
-    private lateinit var sut: UpdateStudyReviewSelectionService
+    private lateinit var sut: UpdateStudyReviewReadingPriorityService
 
     @BeforeEach
     fun setUp() {
         repository = StudyReviewRepositoryFake()
-        sut = UpdateStudyReviewSelectionService(repository)
+        sut = UpdateStudyReviewReadingPriorityService(repository)
     }
 
     @Test
-    @DisplayName("Should change selection status on update: UNCLASSIFIED -> INCLUDED")
-    fun shouldChangeSelectionStatusOnUpdate() {
+    @DisplayName("Should change reading priority on update: LOW -> HIGH")
+    fun shouldChangeReadingStatusOnUpdate() {
         val uuid = UUID.randomUUID()
         val studyId = 1L
-        val status = "INCLUDED"
+        val priority = "HIGH"
 
         val studyReviewDto = createStudyReviewDto(uuid, studyId, repository)
-        val requestModel = createRequestModel(uuid, studyId, status)
+        val requestModel = createRequestModel(uuid, studyId, priority)
 
         val response = sut.changeStatus(requestModel)
         val updatedStudyReview = repository.findById(response.reviewId, response.id)
 
-        assertNotEquals(studyReviewDto.selectionStatus, updatedStudyReview?.selectionStatus)
-        assertEquals(updatedStudyReview?.selectionStatus, status)
+        Assertions.assertNotEquals(studyReviewDto.readingPriority, updatedStudyReview?.readingPriority)
+        Assertions.assertEquals(updatedStudyReview?.readingPriority, priority)
     }
 
 }
---
+++ review/src/test/kotlin/br/all/application/study/update/UpdateStudyReviewSelectionStatusTest.kt
@@ -0,0 +1,42 @@
+package br.all.application.study.update
+
+import br.all.application.repositoryFake.StudyReviewRepositoryFake
+import br.all.application.study.shared.createRequestModel
+import br.all.application.study.shared.createStudyReviewDto
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import java.util.*
+
+class UpdateStudyReviewSelectionStatusTest {
+
+    private lateinit var repository: StudyReviewRepositoryFake
+
+    private lateinit var sut: UpdateStudyReviewSelectionService
+
+    @BeforeEach
+    fun setUp() {
+        repository = StudyReviewRepositoryFake()
+        sut = UpdateStudyReviewSelectionService(repository)
+    }
+
+    @Test
+    @DisplayName("Should change selection status on update: UNCLASSIFIED -> INCLUDED")
+    fun shouldChangeSelectionStatusOnUpdate() {
+        val uuid = UUID.randomUUID()
+        val studyId = 1L
+        val status = "INCLUDED"
+
+        val studyReviewDto = createStudyReviewDto(uuid, studyId, repository)
+        val requestModel = createRequestModel(uuid, studyId, status)
+
+        val response = sut.changeStatus(requestModel)
+        val updatedStudyReview = repository.findById(response.reviewId, response.id)
+
+        assertNotEquals(studyReviewDto.selectionStatus, updatedStudyReview?.selectionStatus)
+        assertEquals(updatedStudyReview?.selectionStatus, status)
+    }
+
+}
