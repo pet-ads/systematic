1504d972772b067210a65636bb7e6500c98a87f6
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-01-04 21:56:59 -0300

    refactor: made presenter mock relaxed to avoid redundant 'every' and extracted repository mocking

--- review/src/test/kotlin/br/all/application/review/find/services/FindOneSystematicStudyServiceImplTest.kt
+++ review/src/test/kotlin/br/all/application/review/find/services/FindOneSystematicStudyServiceImplTest.kt
@@ -3,14 +3,14 @@ package br.all.application.review.find.services
 import br.all.application.researcher.credentials.ResearcherCredentialsService
 import br.all.application.review.find.presenter.FindOneSystematicStudyPresenter
 import br.all.application.review.find.services.FindOneSystematicStudyService.ResponseModel
+import br.all.application.review.repository.SystematicStudyDto
 import br.all.application.review.repository.SystematicStudyRepository
 import br.all.application.review.util.CredentialsServiceMockBuilder.makeResearcherToBeAllowed
 import br.all.application.review.util.TestDataFactory.generateDto
-import io.mockk.Runs
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
-import io.mockk.just
+import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.extension.ExtendWith
@@ -24,12 +24,12 @@ class FindOneSystematicStudyServiceImplTest {
     private lateinit var systematicStudyRepository: SystematicStudyRepository
     @MockK
     private lateinit var credentialsService: ResearcherCredentialsService
-    @MockK
     private lateinit var presenter: FindOneSystematicStudyPresenter
     private lateinit var sut: FindOneSystematicStudyServiceImpl
 
     @BeforeEach
     fun setUp() {
+        presenter = mockk(relaxed = true)
         sut = FindOneSystematicStudyServiceImpl(systematicStudyRepository, credentialsService)
     }
 
@@ -45,14 +45,20 @@ class FindOneSystematicStudyServiceImplTest {
             val response = ResponseModel(researcherId, systematicStudyId, dto)
 
             makeResearcherToBeAllowed(credentialsService, presenter, researcherId)
-            every { systematicStudyRepository.existsById(systematicStudyId) } returns true
-            every { systematicStudyRepository.hasReviewer(systematicStudyId, researcherId) } returns true
-            every { systematicStudyRepository.findById(systematicStudyId) } returns dto
-            every { presenter.prepareSuccessView(response) } just Runs
+            makeSystematicStudyExist(systematicStudyId, researcherId, dto)
 
             sut.findById(presenter, researcherId, systematicStudyId)
-
             verify { presenter.prepareSuccessView(response) }
         }
+    }
+
+    private fun makeSystematicStudyExist(
+        systematicStudyId: UUID,
+        researcherId: UUID,
+        dto: SystematicStudyDto
+    ) {
+        every { systematicStudyRepository.existsById(systematicStudyId) } returns true
+        every { systematicStudyRepository.hasReviewer(systematicStudyId, researcherId) } returns true
+        every { systematicStudyRepository.findById(systematicStudyId) } returns dto
     }
 }
