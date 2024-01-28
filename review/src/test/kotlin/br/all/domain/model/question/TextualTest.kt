ea21f058743ff8650fbfd4d76115eb95bbbc696c
Fábio Seyiji <81870980+Fabio270@users.noreply.github.com>
2024-01-28 17:07:44 -0300

    Textual TEST: should throw IllegalArgumentException for blank answer

--- review/src/test/kotlin/br/all/domain/model/question/TextualTest.kt
+++ review/src/test/kotlin/br/all/domain/model/question/TextualTest.kt
@@ -4,20 +4,21 @@ import br.all.domain.model.protocol.ProtocolId
 import br.all.domain.model.study.Answer
 import io.github.serpro69.kfaker.Faker
 import org.junit.jupiter.api.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import java.util.*
-import kotlin.math.exp
 import kotlin.test.assertEquals
 
 @Tag("UnitTest")
-class TextualTest{
+class TextualTest {
     private val faker = Faker()
 
     @Nested
     @Tag("ValidClasses")
     @DisplayName("When successfully creating Textual questions")
-    inner class WhenSuccessfullyCreatingTextualQuestions{
+    inner class WhenSuccessfullyCreatingTextualQuestions {
         @Test
-        fun `should create a Textual question with valid parameters`(){
+        fun `should create a Textual question with valid parameters`() {
             val id = QuestionId(UUID.randomUUID())
             val protocolId = ProtocolId(UUID.randomUUID())
             val code = faker.lorem.words()
@@ -40,5 +41,20 @@ class TextualTest{
         }
     }
 
-    // TODO: Create answer test 
+    @Nested
+    @Tag("InvalidClasses")
+    @DisplayName("When unable to answer successfully to Textual Questions")
+    inner class WhenUnableToAnswerSuccessfullyToTextualQuestions {
+        @ParameterizedTest(name = "[{index}]: value = \"{0}\"")
+        @ValueSource(strings = ["", " ", "  "])
+        fun `should throw IllegalArgumentException for blank answer`(value: String) {
+            val id = QuestionId(UUID.randomUUID())
+            val protocolId = ProtocolId(UUID.randomUUID())
+            val code = faker.lorem.words()
+            val description = faker.lorem.words()
+            val textual = Textual(id, protocolId, code, description)
+
+            assertThrows<IllegalArgumentException> { textual.answer(value) }
+        }
+    }
 }
