4791cbaf685de46a37c64c70682ab16fd444258b
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-01-29 21:42:05 -0300

    refactor: renamed QuestionDTO to QuestionDto, moved it to repository package and fixed properties

---
+++ review/src/main/kotlin/br/all/application/question/repository/QuestionDto.kt
@@ -0,0 +1,15 @@
+package br.all.application.question.repository
+
+import java.util.*
+
+data class QuestionDto(
+    val questionId: UUID,
+    val protocolId: UUID,
+    val code: String,
+    val description: String,
+    val questionType: String,
+    val scales: Map<String, Int>? = null,
+    val higher: Int? = null,
+    val lower: Int? = null,
+    val options: List<String>? = null,
+)
--- review/src/main/kotlin/br/all/application/question/repository/QuestionRepository.kt
+++ review/src/main/kotlin/br/all/application/question/repository/QuestionRepository.kt
@@ -1,12 +1,11 @@
 package br.all.application.question.repository
 
-import br.all.application.question.create.QuestionDTO
 import br.all.domain.model.protocol.ProtocolId
 import br.all.domain.model.question.QuestionId
 import java.util.UUID
 
 interface QuestionRepository {
-    fun createOrUpdate(dto: QuestionDTO): Any
-    fun findById(systematicStudyId: UUID, id: QuestionId): QuestionDTO?
-    fun findAllByProtocol(systematicStudyId: UUID, protocolId: ProtocolId): List<QuestionDTO>
+    fun createOrUpdate(dto: QuestionDto): Any
+    fun findById(systematicStudyId: UUID, id: QuestionId): QuestionDto?
+    fun findAllByProtocol(systematicStudyId: UUID, protocolId: ProtocolId): List<QuestionDto>
 }
