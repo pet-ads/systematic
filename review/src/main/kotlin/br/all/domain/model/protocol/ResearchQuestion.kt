a33db09cf22ee565a546594c4433bb11eda0ce28
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-01-30 17:15:48 -0300

    refactor: added a custom toString() to ResearchQuestion and added a validation in it

--- review/src/main/kotlin/br/all/domain/model/protocol/ResearchQuestion.kt
+++ review/src/main/kotlin/br/all/domain/model/protocol/ResearchQuestion.kt
@@ -3,6 +3,10 @@ package br.all.domain.model.protocol
 import br.all.domain.shared.ddd.Notification
 import br.all.domain.shared.ddd.ValueObject
 
-data class ResearchQuestion(val description: String) : ValueObject() {
-    override fun validate() = Notification()
+data class ResearchQuestion(private val description: String) : ValueObject() {
+    override fun validate() = Notification().also {
+        if (description.isBlank()) it.addError("The research question description must not be blank!")
+    }
+
+    override fun toString() = description
 }
