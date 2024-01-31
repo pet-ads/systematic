a907ba32257e048bfa34d991702c0cb69af37ecb
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-01-31 12:08:59 -0300

    test: research questions should not be created when providing blank descriptions

--- review/src/test/kotlin/br/all/domain/model/protocol/ResearchQuestionTest.kt
+++ review/src/test/kotlin/br/all/domain/model/protocol/ResearchQuestionTest.kt
@@ -1,6 +1,8 @@
 package br.all.domain.model.protocol
 
 import org.junit.jupiter.api.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 @Tag("UnitClass")
 class ResearchQuestionTest {
@@ -16,6 +18,17 @@ class ResearchQuestionTest {
         @Test
         fun `should convert a valid String to a ResearchQuestion`() {
             assertDoesNotThrow { "Valid description".toResearchQuestion() }
+        }
+    }
+
+    @Nested
+    @Tag("InvalidClasses")
+    @DisplayName("When providing invalid descriptions")
+    inner class WhenProvidingInvalidDescriptions {
+        @ParameterizedTest(name = "[{index}]: description=\"{0}\"")
+        @ValueSource(strings = ["", " ", "   "])
+        fun `should throw when the description is blank`(description: String) {
+            assertThrows<IllegalArgumentException> { ResearchQuestion(description) }
         }
     }
 }
