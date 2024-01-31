7b071520b9b0d1a539cf7c71e4229440d0921631
SauloFernando99 <saulofernando114@gmail.com>
2024-01-31 10:00:05 -0300

    refactor: change protocolId to systematicStudyId

--- review/src/main/kotlin/br/all/application/search/repository/SearchSessionMapper.kt
+++ review/src/main/kotlin/br/all/application/search/repository/SearchSessionMapper.kt
@@ -2,13 +2,14 @@ package br.all.application.search.repository
 
 import br.all.application.search.create.SearchSessionRequestModel
 import br.all.domain.model.protocol.ProtocolId
+import br.all.domain.model.review.SystematicStudyId
 import br.all.domain.model.search.SearchSession
 import br.all.domain.model.search.SearchSessionID
 
-fun SearchSession.Companion.fromRequestModel(sessionId: SearchSessionID, protocolId: ProtocolId, requestModel: SearchSessionRequestModel): SearchSession {
+fun SearchSession.Companion.fromRequestModel(sessionId: SearchSessionID, systematicStudyId: SystematicStudyId, requestModel: SearchSessionRequestModel): SearchSession {
     return SearchSession(
         sessionId,
-        protocolId,
+        systematicStudyId,
         requestModel.searchString,
         requestModel.additionalInfo ?: "",
         source = requestModel.source
