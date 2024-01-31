7b67dc08f485b5c23bf86a5052acb25ef4621d0c
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-01-31 11:54:55 -0300

    feat: applied ResearchQuestion validations in init block

--- review/src/main/kotlin/br/all/domain/model/protocol/ResearchQuestion.kt
+++ review/src/main/kotlin/br/all/domain/model/protocol/ResearchQuestion.kt
@@ -4,6 +4,11 @@ import br.all.domain.shared.ddd.Notification
 import br.all.domain.shared.ddd.ValueObject
 
 data class ResearchQuestion(private val description: String) : ValueObject() {
+    init {
+        val notification = validate()
+        require(notification.hasNoErrors()) { notification.message() }
+    }
+
     override fun validate() = Notification().also {
         if (description.isBlank()) it.addError("The research question description must not be blank!")
     }
