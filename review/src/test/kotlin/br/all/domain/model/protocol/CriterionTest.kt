921940516633c10ceaa70a3e97fbdc6f70943574
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-02-01 17:40:02 -0300

    test: no criteria should be created when providing invalid descriptions

--- review/src/test/kotlin/br/all/domain/model/protocol/CriterionTest.kt
+++ review/src/test/kotlin/br/all/domain/model/protocol/CriterionTest.kt
@@ -4,6 +4,7 @@ import br.all.domain.model.protocol.Criterion.CriterionType
 import org.junit.jupiter.api.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
+import org.junit.jupiter.params.provider.ValueSource
 import kotlin.test.assertEquals
 
 @Tag("UnitTest")
@@ -28,6 +29,20 @@ class CriterionTest {
         fun `should create inclusion criteria with valid description`() {
             val criterion = assertDoesNotThrow { Criterion.toInclude("Valid description") }
             assertEquals(CriterionType.INCLUSION, criterion.type)
+        }
+    }
+
+    @Nested
+    @Tag("InvalidClasses")
+    @DisplayName("When providing invalid descriptions")
+    inner class WhenProvidingInvalidDescriptions {
+        @ParameterizedTest(name = "[{index}]: description=\"{0}\"")
+        @ValueSource(strings = ["", " ", "  "])
+        fun `should not any kind of criteria`(description: String) {
+            assertAll(
+                { assertThrows<IllegalArgumentException> { Criterion(description, CriterionType.INCLUSION) } },
+                { assertThrows<IllegalArgumentException> { Criterion(description, CriterionType.EXCLUSION) } },
+            )
         }
     }
 }
