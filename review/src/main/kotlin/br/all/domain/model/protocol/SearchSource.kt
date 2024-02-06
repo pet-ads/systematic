790b5f2c469e3c05358265eab4e0e7a86107a32e
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-02-06 11:16:42 -0300

    refactor: improved SearchSource code

--- review/src/main/kotlin/br/all/domain/model/protocol/SearchSource.kt
+++ review/src/main/kotlin/br/all/domain/model/protocol/SearchSource.kt
@@ -3,23 +3,24 @@ package br.all.domain.model.protocol
 import br.all.domain.shared.ddd.Notification
 import br.all.domain.shared.ddd.ValueObject
 
-data class SearchSource(val searchSource: String) : ValueObject() {
-
+data class SearchSource(private val value: String) : ValueObject() {
     init {
         val notification = validate()
         require(notification.hasNoErrors()) { notification.message() }
     }
 
-    override fun validate(): Notification {
-        val notification = Notification()
-        val regex = Regex("([A-Z][A-Za-z]+)(\\s*[A-Za-z]+)*")
+    override fun validate() = Notification().also {
+        val regex = Regex("([A-Z][A-Za-z]+)(\\s?[A-Za-z]+)*")
 
-        if (searchSource.isBlank())
-            notification.addError("A search source must not be blank!")
-        if (!(regex matches searchSource))
-            notification.addError("A search source must only contain words separated by white spaces. " +
-                    "Provided: $searchSource")
-
-        return notification
+        if (value.isBlank())
+            it.addError("A search source must not be blank!")
+        if (!(regex matches value))
+            it.addError(
+                "A search source must only contain words separated by white spaces. Provided: $value"
+            )
     }
+
+    override fun toString() = value
 }
+
+fun String.toSearchSource() = SearchSource(this)
