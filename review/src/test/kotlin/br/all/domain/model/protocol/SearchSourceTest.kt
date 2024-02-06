4ac1084ddf03a5ef19ff7200b4bdef0f9234e326
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-02-06 11:22:25 -0300

    test: improved SearchSourceTest by creating nested classes and correctly tagging them

--- review/src/test/kotlin/br/all/domain/model/protocol/SearchSourceTest.kt
+++ review/src/test/kotlin/br/all/domain/model/protocol/SearchSourceTest.kt
@@ -1,25 +1,34 @@
 package br.all.domain.model.protocol
 
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertDoesNotThrow
-import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EmptySource
 
-internal class SearchSourceTest {
-    @Test
-    fun `Should successfully create a valid search source`() {
-        assertDoesNotThrow { SearchSource("Valid SearchSource") }
+@Tag("UnitTest")
+class SearchSourceTest {
+    @Nested
+    @Tag("ValidClasses")
+    @DisplayName("When the given source is valid")
+    inner class WhenTheGivenSourceIsValid {
+        @Test
+        fun `Should successfully create a valid search source`() {
+            assertDoesNotThrow { SearchSource("Valid SearchSource") }
+        }
     }
 
-    @Test
-    fun `Should throw if search source contains digits or symbols`() {
-        assertThrows<IllegalArgumentException> { SearchSource("H& @ba O-O Dxd5 1m J0ta") }
-    }
+    @Nested
+    @Tag("InvalidClasses")
+    @DisplayName("When invalid sources are given")
+    inner class WhenInvalidSourcesAreGiven {
+        @Test
+        fun `Should throw if search source contains digits or symbols`() {
+            assertThrows<IllegalArgumentException> { SearchSource("H& @ba O-O Dxd5 1m J0ta") }
+        }
 
-    @ParameterizedTest(name = "[{index}] source=\"{0}\"")
-    @EmptySource
-    fun `Should throw to any kind of empty search source`(source: String) {
-        assertThrows<IllegalArgumentException> { SearchSource(source) }
+        @ParameterizedTest(name = "[{index}] source=\"{0}\"")
+        @EmptySource
+        fun `Should throw to any kind of empty search source`(source: String) {
+            assertThrows<IllegalArgumentException> { SearchSource(source) }
+        }
     }
 }
