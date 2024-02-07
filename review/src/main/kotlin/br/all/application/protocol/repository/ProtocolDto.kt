4c142130e566751eb293bda4c43156631274ea89
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-02-07 20:32:26 -0300

    fix: fixed ProtocolDto types

--- review/src/main/kotlin/br/all/application/protocol/repository/ProtocolDto.kt
+++ review/src/main/kotlin/br/all/application/protocol/repository/ProtocolDto.kt
@@ -6,24 +6,24 @@ data class ProtocolDto(
     val id: UUID,
     val systematicStudy: UUID,
 
-    val goal: String,
-    val justification: String,
+    val goal: String?,
+    val justification: String?,
 
     val researchQuestions: Set<String>,
     val keywords: Set<String>,
-    val searchString: String,
+    val searchString: String?,
     val informationSources: Set<String>,
-    val sourcesSelectionCriteria: String,
+    val sourcesSelectionCriteria: String?,
 
-    val searchMethod: String,
+    val searchMethod: String?,
     val studiesLanguages: Set<String>,
-    val studyTypeDefinition: String,
+    val studyTypeDefinition: String?,
 
-    val selectionProcess: String,
+    val selectionProcess: String?,
     val selectionCriteria: Set<Pair<String, String>>,
 
-    val dataCollectionProcess: String,
-    val analysisAndSynthesisProcess: String,
+    val dataCollectionProcess: String?,
+    val analysisAndSynthesisProcess: String?,
 
     val extractionQuestions: Set<UUID>,
     val robQuestions: Set<UUID>,
