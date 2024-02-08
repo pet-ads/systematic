e85bf76684d5bcdf4d83a1de9d519ae417f2cf40
lucas-ifsp <lucas.ruas@gmail.com>
2024-02-08 20:57:32 -0300

    feat(question): split create question endpoint by question type

--- web/src/main/kotlin/br/all/question/controller/ExtractionQuestionController.kt
+++ web/src/main/kotlin/br/all/question/controller/ExtractionQuestionController.kt
@@ -1,9 +1,10 @@
 package br.all.question.controller
 
 import br.all.application.question.create.CreateQuestionService
+import br.all.application.question.create.CreateQuestionService.RequestModel
 import br.all.application.question.find.FindQuestionService
-import br.all.question.presenter.riskOfBias.RestfulCreateRoBQuestionPresenter
-import br.all.question.presenter.riskOfBias.RestfulFindRoBQuestionPresenter
+import br.all.question.presenter.extraction.RestfulFindExtractionQuestionPresenter
+import br.all.question.presenter.extraction.RestfulCreateExtractionQuestionPresenter
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -13,28 +14,85 @@ import br.all.application.question.create.CreateQuestionService.RequestModel as 
 @RestController
 @RequestMapping("/api/v1/researcher/{researcherId}/systematic-study/{systematicStudyId}/protocol/extraction-question")
 class ExtractionQuestionController(
-    val createQuestionService: CreateQuestionService,
-    val findOneService: FindQuestionService
+    val createQuestionService: CreateQuestionService, val findOneService: FindQuestionService
 ) {
-    @PostMapping
-    fun createQuestion(
-        @PathVariable researcherId: UUID,
-        @PathVariable systematicStudyId: UUID,
-        @RequestBody request: CreateRequest,
-    ): ResponseEntity<*> {
-        val presenter = RestfulCreateRoBQuestionPresenter()
-        createQuestionService.create(presenter, request)
+    data class TextualRequest(val code: String, val description: String)
+    data class PickListRequest(val code: String, val description: String, val options: List<String>)
+    data class LabeledScaleRequest(val code: String, val description: String, val scales: Map<String, Int>)
+    data class NumberScaleRequest(val code: String, val description: String, val lower: Int, val higher: Int)
 
+    fun createQuestion(request: CreateRequest): ResponseEntity<*> {
+        val presenter = RestfulCreateExtractionQuestionPresenter()
+        createQuestionService.create(presenter, request)
         return presenter.responseEntity ?: ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
-    @GetMapping("/{code}")
+    @PostMapping("/textual")
+    fun createTextualQuestion(
+        @PathVariable researcherId: UUID, @PathVariable systematicStudyId: UUID, @RequestBody request: TextualRequest,
+    ): ResponseEntity<*> = createQuestion(
+        RequestModel(
+            researcherId,
+            systematicStudyId, "Textual",
+            request.code,
+            request.description
+        )
+    )
+
+    @PostMapping("/pick-list")
+    fun createPickListQuestion(
+        @PathVariable researcherId: UUID, @PathVariable systematicStudyId: UUID, @RequestBody request: PickListRequest,
+    ): ResponseEntity<*> = createQuestion(
+        RequestModel(
+            researcherId,
+            systematicStudyId,
+            "PickList",
+            request.code,
+            request.description,
+            options = request.options
+        )
+    )
+
+    @PostMapping("/labeled-scale")
+    fun createLabeledScaleQuestion(
+        @PathVariable researcherId: UUID,
+        @PathVariable systematicStudyId: UUID,
+        @RequestBody request: LabeledScaleRequest,
+    ): ResponseEntity<*> = createQuestion(
+        RequestModel(
+            researcherId,
+            systematicStudyId,
+            "LabeledScale",
+            request.code,
+            request.description,
+            scales = request.scales
+        )
+    )
+
+    @PostMapping("/number-scale")
+    fun createNumberScaleQuestion(
+        @PathVariable researcherId: UUID,
+        @PathVariable systematicStudyId: UUID,
+        @RequestBody request: NumberScaleRequest,
+    ): ResponseEntity<*> = createQuestion(
+        RequestModel(
+            researcherId,
+            systematicStudyId,
+            "LabeledScale",
+            request.code,
+            request.description,
+            higher = request.higher,
+            lower = request.lower
+        )
+    )
+
+    @GetMapping("/{questionId}")
     fun findQuestion(
         @PathVariable researcherId: UUID,
         @PathVariable systematicStudyId: UUID,
         @PathVariable questionId: UUID,
     ): ResponseEntity<*> {
-        val presenter = RestfulFindRoBQuestionPresenter()
+        val presenter = RestfulFindExtractionQuestionPresenter()
         val request = FindQuestionService.RequestModel(researcherId, systematicStudyId, questionId)
         findOneService.findOne(presenter, request)
         return presenter.responseEntity ?: ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR)
