fc1a445273d5a9352054ee523ed776f3bc91f617
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-02-09 19:53:36 -0300

    feat: search sources now treats its values to keep the first letters capitalized

--- review/src/main/kotlin/br/all/domain/model/protocol/SearchSource.kt
+++ review/src/main/kotlin/br/all/domain/model/protocol/SearchSource.kt
@@ -3,14 +3,15 @@ package br.all.domain.model.protocol
 import br.all.domain.shared.ddd.Notification
 import br.all.domain.shared.ddd.ValueObject
 
-data class SearchSource(private val value: String) : ValueObject() {
+data class SearchSource(private var value: String) : ValueObject() {
     init {
+        value = value.replace("(^|\\s)[a-z]".toRegex()) { it.value.uppercase() }
         val notification = validate()
         require(notification.hasNoErrors()) { notification.message() }
     }
 
     override fun validate() = Notification().also {
-        val regex = Regex("([A-Z][A-Za-z]+)(\\s?[A-Za-z]+)*")
+        val regex = Regex("([a-z]+)(\\s[a-z]+)*", RegexOption.IGNORE_CASE)
 
         if (value.isBlank())
             it.addError("A search source must not be blank!")
