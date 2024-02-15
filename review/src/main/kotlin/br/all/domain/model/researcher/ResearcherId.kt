967bc03885c1519c4a29a3aceb8c2ef83047073a
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-02-15 17:36:49 -0300

    refactor: added extension function to UUID to convert it to ResearcherId

--- review/src/main/kotlin/br/all/domain/model/researcher/ResearcherId.kt
+++ review/src/main/kotlin/br/all/domain/model/researcher/ResearcherId.kt
@@ -2,12 +2,16 @@ package br.all.domain.model.researcher
 
 import br.all.domain.shared.ddd.Identifier
 import br.all.domain.shared.ddd.Notification
-import java.util.UUID
+import java.util.*
 
 @JvmInline
 value class ResearcherId(val value : UUID ) : Identifier <UUID> {
 
     override fun validate() = Notification()
+
     override fun value(): UUID = value
+
     override fun toString() = value.toString()
 }
+
+fun UUID.toResearcherId() = ResearcherId(UUID.randomUUID())
