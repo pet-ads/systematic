6221de93e449acf75415ca2a915357b4afa5c43d
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-03-06 16:59:45 -0300

    style: added blank lines to improve ProtocolId code style

--- review/src/main/kotlin/br/all/domain/model/protocol/ProtocolId.kt
+++ review/src/main/kotlin/br/all/domain/model/protocol/ProtocolId.kt
@@ -7,6 +7,8 @@ import java.util.*
 @JvmInline
 value class ProtocolId(private val value: UUID) : Identifier<UUID> {
     override fun validate() = Notification()
+
     override fun value(): UUID = value
+
     override fun toString() = value.toString()
 }
