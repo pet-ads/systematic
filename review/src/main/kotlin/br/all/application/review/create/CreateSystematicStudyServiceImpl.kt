139282cb0be7601f2cbadc6864d146c9cca12b72
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-03-07 19:53:26 -0300

    feat: protocol now is created together with its systematic study

--- review/src/main/kotlin/br/all/application/review/create/CreateSystematicStudyServiceImpl.kt
+++ review/src/main/kotlin/br/all/application/review/create/CreateSystematicStudyServiceImpl.kt
@@ -1,5 +1,7 @@
 package br.all.application.review.create
 
+import br.all.application.protocol.repository.ProtocolRepository
+import br.all.application.protocol.repository.toDto
 import br.all.application.researcher.credentials.ResearcherCredentialsService
 import br.all.application.review.create.CreateSystematicStudyService.RequestModel
 import br.all.application.review.create.CreateSystematicStudyService.ResponseModel
@@ -7,26 +9,33 @@ import br.all.application.review.repository.SystematicStudyRepository
 import br.all.application.review.repository.fromRequestModel
 import br.all.application.review.repository.toDto
 import br.all.application.shared.presenter.PreconditionChecker
+import br.all.domain.model.protocol.Protocol
 import br.all.domain.model.researcher.ResearcherId
 import br.all.domain.model.review.SystematicStudy
+import br.all.domain.model.review.toSystematicStudyId
 import br.all.domain.services.UuidGeneratorService
 import java.util.*
 
 class CreateSystematicStudyServiceImpl(
-    private val repository: SystematicStudyRepository,
+    private val systematicStudyRepository: SystematicStudyRepository,
+    private val protocolRepository: ProtocolRepository,
     private val uuidGeneratorService: UuidGeneratorService,
     private val credentialsService: ResearcherCredentialsService,
 ): CreateSystematicStudyService {
     override fun create(presenter: CreateSystematicStudyPresenter, researcher: UUID, request: RequestModel) {
-        PreconditionChecker(repository, credentialsService).also {
+        PreconditionChecker(systematicStudyRepository, credentialsService).also {
             it.prepareIfUnauthenticatedOrUnauthorized(presenter, ResearcherId(researcher))
         }
         if (presenter.isDone()) return
 
         val id = uuidGeneratorService.next()
         SystematicStudy.fromRequestModel(id, researcher, request).also {
-            repository.saveOrUpdate(it.toDto())
+            systematicStudyRepository.saveOrUpdate(it.toDto())
         }
+
+        val protocol = Protocol.write(id.toSystematicStudyId(), emptySet())
+            .build()
+        protocolRepository.saveOrUpdate(protocol.toDto())
 
         presenter.prepareSuccessView(ResponseModel(researcher, id))
     }
