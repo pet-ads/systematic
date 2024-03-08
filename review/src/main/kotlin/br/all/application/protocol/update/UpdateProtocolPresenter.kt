17dc2b2ddb092d0f54fd1c5afa36c63e540bc6ec
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-03-08 17:14:27 -0300

    style: style improvements to follow kotlin convention

--- review/src/main/kotlin/br/all/application/protocol/update/UpdateProtocolPresenter.kt
+++ review/src/main/kotlin/br/all/application/protocol/update/UpdateProtocolPresenter.kt
@@ -3,4 +3,4 @@ package br.all.application.protocol.update
 import br.all.application.protocol.update.UpdateProtocolService.ResponseModel
 import br.all.application.shared.presenter.GenericPresenter
 
-interface UpdateProtocolPresenter: GenericPresenter<ResponseModel>
+interface UpdateProtocolPresenter : GenericPresenter<ResponseModel>
