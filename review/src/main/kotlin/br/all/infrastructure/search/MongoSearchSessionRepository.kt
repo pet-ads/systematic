6fb825449a52455eb2d292e1ac90d9e1dce92732
SauloFernando99 <saulofernando114@gmail.com>
2024-03-10 20:21:08 -0300

    feat: new fun for findAll

--- review/src/main/kotlin/br/all/infrastructure/search/MongoSearchSessionRepository.kt
+++ review/src/main/kotlin/br/all/infrastructure/search/MongoSearchSessionRepository.kt
@@ -1,9 +1,11 @@
 package br.all.infrastructure.search
 
 import br.all.application.search.repository.SearchSessionDto
+import br.all.infrastructure.study.StudyReviewDocument
 import org.springframework.data.mongodb.repository.MongoRepository
 import java.util.UUID
 
 interface MongoSearchSessionRepository : MongoRepository<SearchSessionDocument, UUID>{
     fun existsBySystematicStudyIdAndSource(systematicStudyId: UUID, source: String): Boolean
+    fun findAllById_SystematicStudyId(systematicStudyId: UUID): List<SearchSessionDocument>
 }
