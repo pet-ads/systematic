d31055d03e3582ab0975bb184d4e1eb2476eedcc
SauloFernando99 <saulofernando114@gmail.com>
2024-03-10 10:31:23 -0300

    feat: service interface for findAllSearchSessions

--- review/src/main/kotlin/br/all/application/search/find/service/FindAllSearchSessionsService.kt
+++ review/src/main/kotlin/br/all/application/search/find/service/FindAllSearchSessionsService.kt
@@ -1,4 +1,20 @@
 package br.all.application.search.find.service
 
+import br.all.application.search.find.presenter.FindAllSearchSessionsPresenter
+import br.all.application.search.repository.SearchSessionDto
+import java.util.*
+
 interface FindAllSearchSessionsService {
+    fun findAllSearchSessionsPresenter(presenter: FindAllSearchSessionsPresenter, request: RequestModel)
+
+    data class RequestModel(
+        val researcherId: UUID,
+        val systematicStudyId: UUID
+    )
+
+    data class ResponseModel(
+        val researcherId: UUID,
+        val systematicStudyId: UUID,
+        val searchSessions: List<SearchSessionDto>
+    )
 }
