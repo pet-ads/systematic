29774015f1b36212412622e139c4a8e850e25767
SauloFernando99 <saulofernando114@gmail.com>
2024-03-11 19:43:33 -0300

    feat: implemented presenter and service for FindSearchSession

---
+++ review/src/main/kotlin/br/all/application/search/find/presenter/FindSearchSessionPresenter.kt
@@ -0,0 +1,6 @@
+package br.all.application.search.find.presenter
+
+import br.all.application.search.find.service.FindSearchSessionService.RequestModel
+import br.all.application.shared.presenter.GenericPresenter
+
+interface FindSearchSessionPresenter: GenericPresenter<RequestModel>
---
+++ review/src/main/kotlin/br/all/application/search/find/service/FindSearchSessionService.kt
@@ -0,0 +1,20 @@
+package br.all.application.search.find.service
+
+import br.all.application.search.find.presenter.FindSearchSessionPresenter
+import br.all.application.search.repository.SearchSessionDto
+import java.util.UUID
+
+interface FindSearchSessionService {
+    fun findOneSession(presenter: FindSearchSessionPresenter, request: RequestModel)
+
+    data class RequestModel(
+        val researcherId: UUID,
+        val systematicStudyId: UUID,
+        val sessionId: UUID
+    )
+
+    data class ResponseModel(
+        val sessionId: UUID,
+        val content: SearchSessionDto
+    )
+}
