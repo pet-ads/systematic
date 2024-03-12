37f0aaa077800d1e23dd0dc9b979ff5fa11a35b2
SauloFernando99 <saulofernando114@gmail.com>
2024-03-11 21:05:06 -0300

    feat: implemented full FindSearchSessionServiceImpl class

--- review/src/main/kotlin/br/all/application/search/find/service/FindSearchSessionServiceImpl.kt
+++ review/src/main/kotlin/br/all/application/search/find/service/FindSearchSessionServiceImpl.kt
@@ -1,4 +1,37 @@
 package br.all.application.search.find.service
 
-class FindSearchSessionServiceImpl {
+import br.all.application.researcher.credentials.ResearcherCredentialsService
+import br.all.application.review.repository.SystematicStudyRepository
+import br.all.application.search.find.presenter.FindSearchSessionPresenter
+import br.all.application.search.find.service.FindSearchSessionService.RequestModel
+import br.all.application.search.find.service.FindSearchSessionService.ResponseModel
+import br.all.application.search.repository.SearchSessionRepository
+import br.all.application.shared.exceptions.EntityNotFoundException
+import br.all.application.shared.presenter.PreconditionChecker
+import br.all.domain.model.researcher.ResearcherId
+import br.all.domain.model.review.SystematicStudyId
+
+class FindSearchSessionServiceImpl (
+    private val systematicStudyRepository: SystematicStudyRepository,
+    private val searchSessionRepository: SearchSessionRepository,
+    private val credentialsService: ResearcherCredentialsService,
+) : FindSearchSessionService {
+
+    override fun findOneSession(presenter: FindSearchSessionPresenter, request: RequestModel) {
+        val researcherId = ResearcherId(request.researcherId)
+        val systematicStudyId = SystematicStudyId(request.systematicStudyId)
+        val preconditionChecker = PreconditionChecker(systematicStudyRepository, credentialsService)
+        preconditionChecker.prepareIfViolatesPreconditions(presenter, researcherId, systematicStudyId)
+
+        if(presenter.isDone()) return
+
+        val searchSession = searchSessionRepository.findById(request.sessionId)
+
+        if (searchSession == null){
+            val message = "There is no search session of id ${request.sessionId}"
+            presenter.prepareFailView(EntityNotFoundException(message))
+            return
+        }
+        presenter.prepareSuccessView((ResponseModel(request.researcherId, searchSession)))
+    }
 }
