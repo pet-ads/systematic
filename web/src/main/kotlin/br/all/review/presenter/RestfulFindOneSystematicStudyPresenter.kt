f0c5d4a6aae1348b4a363e1995c32412ddf5ff38
biankyrou <bianca.carvalio@aluno.ifsp.edu.br>
2024-03-18 15:08:18 -0300

    att-links-presenter

--- web/src/main/kotlin/br/all/review/presenter/RestfulFindOneSystematicStudyPresenter.kt
+++ web/src/main/kotlin/br/all/review/presenter/RestfulFindOneSystematicStudyPresenter.kt
@@ -1,8 +1,11 @@
 package br.all.review.presenter
 
+import br.all.application.review.create.CreateSystematicStudyService
 import br.all.application.review.find.presenter.FindOneSystematicStudyPresenter
 import br.all.application.review.find.services.FindOneSystematicStudyService.ResponseModel
 import br.all.application.review.repository.SystematicStudyDto
+import br.all.application.review.update.services.UpdateSystematicStudyService
+import br.all.application.study.create.CreateStudyReviewService
 import br.all.review.controller.SystematicStudyController
 import br.all.shared.error.createErrorResponseFrom
 import org.springframework.hateoas.RepresentationModel
@@ -10,6 +13,8 @@ import org.springframework.hateoas.server.mvc.linkTo
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.PathVariable
+import java.util.*
 
 @Component
 class RestfulFindOneSystematicStudyPresenter: FindOneSystematicStudyPresenter {
@@ -25,6 +30,24 @@ class RestfulFindOneSystematicStudyPresenter: FindOneSystematicStudyPresenter {
         restfulResponse.add(self)
         responseEntity = ResponseEntity.status(HttpStatus.OK).body(restfulResponse)
     }
+
+    private fun postSystematicStudy(researcherId: UUID) = linkTo<SystematicStudyController> {
+        postSystematicStudy(
+            researcherId,
+            request = CreateSystematicStudyService.RequestModel(
+                "title",
+                "description",
+                collaborators = setOf(UUID.randomUUID())))
+    }.withSelfRel()
+
+    private fun updateSystematicStudy(researcherId: UUID, systematicStudyId: UUID) = linkTo<SystematicStudyController> {
+        updateSystematicStudy(
+            researcherId,
+            systematicStudyId,
+            request = UpdateSystematicStudyService.RequestModel(
+                "title",
+                "description"))
+    }.withSelfRel()
 
     override fun prepareFailView(throwable: Throwable)= run { responseEntity = createErrorResponseFrom(throwable) }
 
