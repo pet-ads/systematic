e658f5fa42327135ffead5afdb0c4ddecb77c17c
SauloFernando99 <saulofernando114@gmail.com>
2024-03-24 22:23:14 -0300

    feat: made additionalInfo nullable

---
+++ review/src/main/kotlin/br/all/application/search/update/UpdateSearchSessionPresenter.kt
@@ -0,0 +1,4 @@
+package br.all.application.search.update
+
+interface UpdateSearchSessionPresenter {
+}
---
+++ review/src/test/kotlin/br/all/application/search/update/UpdateSearchSessionServiceImplTest.kt
@@ -0,0 +1,4 @@
+package br.all.application.search.update
+
+class UpdateSearchSessionServiceImplTest {
+}
