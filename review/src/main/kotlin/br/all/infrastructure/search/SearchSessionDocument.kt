d0789c2291ebb99a275cfe3a1397972c9db4e6f0
SauloFernando99 <saulofernando114@gmail.com>
2024-03-25 08:16:51 -0300

    feat: additional info now is nullable

--- review/src/main/kotlin/br/all/infrastructure/search/SearchSessionDocument.kt
+++ review/src/main/kotlin/br/all/infrastructure/search/SearchSessionDocument.kt
@@ -11,7 +11,7 @@ data class SearchSessionDocument(
     val id: UUID,
     val systematicStudyId: UUID,
     val searchString: String,
-    val additionalInfo: String,
+    val additionalInfo: String?,
     val timestamp: LocalDateTime,
     val source: String
 )
