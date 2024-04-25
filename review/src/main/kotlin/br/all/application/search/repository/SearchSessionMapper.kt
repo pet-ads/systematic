eda1bcc65ae46fd5001b85cc69cd8cf3ddd2450e
SauloFernando99 <saulofernando114@gmail.com>
2024-04-25 10:27:52 -0300

    feat: included the researcherId in each function

--- review/src/main/kotlin/br/all/application/search/repository/SearchSessionMapper.kt
+++ review/src/main/kotlin/br/all/application/search/repository/SearchSessionMapper.kt
@@ -2,6 +2,7 @@ package br.all.application.search.repository
 import br.all.application.search.create.CreateSearchSessionService.RequestModel
 import br.all.application.search.update.UpdateSearchSessionService
 import br.all.domain.model.protocol.SearchSource
+import br.all.domain.model.researcher.ResearcherId
 import br.all.domain.model.review.SystematicStudyId
 import br.all.domain.model.search.SearchSession
 import br.all.domain.model.search.SearchSessionID
@@ -10,6 +11,7 @@ import java.time.LocalDateTime
 fun SearchSession.toDto() = SearchSessionDto(
     id.value(),
     systematicStudyId.value(),
+    researcherId.value(),
     searchString,
     additionalInfo,
     timestamp,
@@ -19,6 +21,7 @@ fun SearchSession.toDto() = SearchSessionDto(
 fun SearchSession.Companion.fromRequestModel(id: SearchSessionID, request: RequestModel) = SearchSession(
     id,
     SystematicStudyId(request.systematicStudyId),
+    ResearcherId(request.researcherId),
     request.searchString,
     request.additionalInfo ?: "",
     source = SearchSource(request.source),
@@ -27,6 +30,7 @@ fun SearchSession.Companion.fromRequestModel(id: SearchSessionID, request: Reque
 fun SearchSession.Companion.fromDto(dto: SearchSessionDto) = SearchSession(
     SearchSessionID(dto.id),
     SystematicStudyId(dto.systematicStudyId),
+    ResearcherId(dto.researcherId),
     dto.searchString,
     dto.additionalInfo,
     dto.timestamp,
