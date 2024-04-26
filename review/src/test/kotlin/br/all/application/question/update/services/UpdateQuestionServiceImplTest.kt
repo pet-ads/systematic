1ba9cd214df971978323aade9b45ad433f4d6d37
Fábio Seyiji <81870980+Fabio270@users.noreply.github.com>
2024-04-26 09:41:24 -0300

    Test: added valid classes tests for all types of questions and their particular attributes

--- review/src/test/kotlin/br/all/application/question/update/services/UpdateQuestionServiceImplTest.kt
+++ review/src/test/kotlin/br/all/application/question/update/services/UpdateQuestionServiceImplTest.kt
@@ -1,6 +1,7 @@
 package br.all.application.question.update.services
 
-import br.all.application.question.create.CreateQuestionService
+import br.all.application.question.create.CreateQuestionService.*
+import br.all.application.question.create.CreateQuestionService.QuestionType.*
 import br.all.application.question.repository.QuestionRepository
 import br.all.application.question.update.presenter.UpdateQuestionPresenter
 import br.all.application.question.util.TestDataFactory
@@ -63,13 +64,13 @@ class UpdateQuestionServiceImplTest {
         fun setUp() = run { preconditionCheckerMocking.makeEverythingWork() }
 
         @ParameterizedTest
-        @EnumSource(CreateQuestionService.QuestionType::class)
-        fun `should successfully update a question code`(questionType: CreateQuestionService.QuestionType) {
+        @EnumSource(QuestionType::class)
+        fun `should successfully update a question code`(questionType: QuestionType) {
             val dto = when (questionType) {
-                CreateQuestionService.QuestionType.TEXTUAL -> factory.generateTextualDto()
-                CreateQuestionService.QuestionType.NUMBERED_SCALE -> factory.generateNumberedScaleDto()
-                CreateQuestionService.QuestionType.PICK_LIST -> factory.generatePickListDto()
-                CreateQuestionService.QuestionType.LABELED_SCALE -> factory.generateLabeledScaleDto()
+                TEXTUAL -> factory.generateTextualDto()
+                NUMBERED_SCALE -> factory.generateNumberedScaleDto()
+                PICK_LIST -> factory.generatePickListDto()
+                LABELED_SCALE -> factory.generateLabeledScaleDto()
             }
             val updatedDto = dto.copy(code = "new code")
             val request = factory.updateQuestionRequestModel(updatedDto, questionType)
@@ -85,13 +86,13 @@ class UpdateQuestionServiceImplTest {
         }
 
         @ParameterizedTest
-        @EnumSource(CreateQuestionService.QuestionType::class)
-        fun `should successfully update a question description`(questionType: CreateQuestionService.QuestionType) {
+        @EnumSource(QuestionType::class)
+        fun `should successfully update a question description`(questionType: QuestionType) {
             val dto = when (questionType) {
-                CreateQuestionService.QuestionType.TEXTUAL -> factory.generateTextualDto()
-                CreateQuestionService.QuestionType.NUMBERED_SCALE -> factory.generateNumberedScaleDto()
-                CreateQuestionService.QuestionType.PICK_LIST -> factory.generatePickListDto()
-                CreateQuestionService.QuestionType.LABELED_SCALE -> factory.generateLabeledScaleDto()
+                TEXTUAL -> factory.generateTextualDto()
+                NUMBERED_SCALE -> factory.generateNumberedScaleDto()
+                PICK_LIST -> factory.generatePickListDto()
+                LABELED_SCALE -> factory.generateLabeledScaleDto()
             }
             val updatedDto = dto.copy(description = "new description")
             val request = factory.updateQuestionRequestModel(updatedDto, questionType)
@@ -105,5 +106,69 @@ class UpdateQuestionServiceImplTest {
                 presenter.prepareSuccessView(response)
             }
         }
+
+        @Test
+        fun `should successfully update a picklist question list of options`() {
+            val dto = factory.generatePickListDto()
+            val updatedDto = dto.copy(options = listOf("new option 1", " new option 2"))
+            val request = factory.updateQuestionRequestModel(updatedDto, PICK_LIST)
+            val response = factory.updateQuestionResponseModel()
+            every { repository.findById(factory.systematicStudy, factory.question) } returns dto
+            sut.update(presenter, request)
+
+            verify {
+                repository.createOrUpdate(updatedDto)
+                presenter.prepareSuccessView(response)
+            }
+        }
+
+        @Test
+        fun `should successfully update a labeled-scale question map of labels`() {
+            val dto = factory.generateLabeledScaleDto()
+            val updatedDto = dto.copy(scales = mapOf("new label 1" to 1, "new label 1" to 2))
+            val request = factory.updateQuestionRequestModel(updatedDto, LABELED_SCALE)
+            val response = factory.updateQuestionResponseModel()
+            every { repository.findById(factory.systematicStudy, factory.question) } returns dto
+            sut.update(presenter, request)
+            verify {
+                repository.createOrUpdate(updatedDto)
+                presenter.prepareSuccessView(response)
+            }
+        }
+
+        @Test
+        fun `should successfully update a numbered-scale question higher boundary if it is bigger than lower boundary value`() {
+            val dto = factory.generateNumberedScaleDto()
+            val updatedDto = dto.copy(higher = 11)
+            val request = factory.updateQuestionRequestModel(updatedDto, NUMBERED_SCALE)
+            val response = factory.updateQuestionResponseModel()
+            every { repository.findById(factory.systematicStudy, factory.question) } returns dto
+            sut.update(presenter, request)
+            verify {
+                repository.createOrUpdate(updatedDto)
+                presenter.prepareSuccessView(response)
+            }
+        }
+
+        @Test
+        fun `should successfully update a numbered-scale question lower boundary if it is smaller than bigger boundary value`() {
+            val dto = factory.generateNumberedScaleDto()
+            val updatedDto = dto.copy(lower = 2)
+            val request = factory.updateQuestionRequestModel(updatedDto, NUMBERED_SCALE)
+            val response = factory.updateQuestionResponseModel()
+            every { repository.findById(factory.systematicStudy, factory.question) } returns dto
+            sut.update(presenter, request)
+            verify {
+                repository.createOrUpdate(updatedDto)
+                presenter.prepareSuccessView(response)
+            }
+        }
+    }
+
+    @Nested
+    @Tag("InvalidClasses")
+    @DisplayName("When not able to update a question")
+    inner class WhenNotAbleToUpdateAQuestion{
+
     }
 }
