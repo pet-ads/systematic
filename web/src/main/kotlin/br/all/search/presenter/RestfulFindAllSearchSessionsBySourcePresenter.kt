fa23901c2767d30939486fa9f8c999449914e806
SauloFernando99 <saulofernando114@gmail.com>
2024-04-30 19:28:39 -0300

    feat: implemented the presenter for find all search sessions by source

--- web/src/main/kotlin/br/all/search/presenter/RestfulFindAllSearchSessionsBySourcePresenter.kt
+++ web/src/main/kotlin/br/all/search/presenter/RestfulFindAllSearchSessionsBySourcePresenter.kt
@@ -1,4 +1,47 @@
 package br.all.search.presenter
 
-class RestifulFindAllSearchSessionsBySourcePresenter {
+import br.all.application.search.find.presenter.FindAllSearchSessionsBySourcePresenter
+import br.all.application.search.find.service.FindAllSearchSessionsBySourceService.ResponseModel
+import br.all.application.search.find.service.FindAllSearchSessionsService
+import br.all.application.search.repository.SearchSessionDto
+import br.all.search.controller.SearchSessionController
+import br.all.shared.error.createErrorResponseFrom
+import org.springframework.hateoas.RepresentationModel
+import org.springframework.hateoas.server.mvc.linkTo
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class RestfulFindAllSearchSessionsBySourcePresenter: FindAllSearchSessionsBySourcePresenter {
+
+    var responseEntity: ResponseEntity<*>? = null
+
+    override fun prepareSuccessView(response: ResponseModel) {
+        val restfulResponse = ViewModel(
+            response.systematicStudyId,
+            response.searchSource,
+            response.searchSessions.size,
+            response.searchSessions
+        )
+
+        val self = linkTo<SearchSessionController> {
+            findSearchSessionsBySource(response.researcherId, response.systematicStudyId, response.searchSource)
+        }.withSelfRel()
+
+        restfulResponse.add(self)
+        responseEntity = ResponseEntity.status(HttpStatus.OK).body(restfulResponse)
+
+    }
+
+    override fun prepareFailView(throwable: Throwable) = run { responseEntity = createErrorResponseFrom(throwable) }
+
+    override fun isDone() = responseEntity != null
+    private data class ViewModel (
+        val systematicStudyId: UUID,
+        val searchSource: String,
+        val size: Int,
+        val searchSessions: List<SearchSessionDto>
+    ) : RepresentationModel<ViewModel>()
 }
