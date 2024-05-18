85febc90057e4300a1f118decb7812628c4565de
SauloFernando99 <saulofernando114@gmail.com>
2024-05-18 20:14:25 -0300

    refactor: switched the old ResearcherCredentialService for the new CredentialService

--- review/src/main/kotlin/br/all/application/search/find/service/FindAllSearchSessionsBySourceServiceImpl.kt
+++ review/src/main/kotlin/br/all/application/search/find/service/FindAllSearchSessionsBySourceServiceImpl.kt
@@ -2,25 +2,31 @@ package br.all.application.search.find.service
 
 import br.all.application.user.credentials.ResearcherCredentialsService
 import br.all.application.review.repository.SystematicStudyRepository
+import br.all.application.review.repository.fromDto
 import br.all.application.search.find.presenter.FindAllSearchSessionsBySourcePresenter
 import br.all.application.search.find.service.FindAllSearchSessionsBySourceService.*
 import br.all.application.search.repository.SearchSessionRepository
 import br.all.application.shared.presenter.PreconditionChecker
+import br.all.application.shared.presenter.prepareIfFailsPreconditions
+import br.all.application.user.CredentialsService
 import br.all.domain.model.researcher.ResearcherId
+import br.all.domain.model.review.SystematicStudy
 import br.all.domain.model.review.SystematicStudyId
-
+//TODO finish
 class FindAllSearchSessionsBySourceServiceImpl(
     private val systematicStudyRepository: SystematicStudyRepository,
     private val searchSessionRepository: SearchSessionRepository,
-    private val credentialsService: ResearcherCredentialsService,
+    private val credentialsService: CredentialsService,
 ): FindAllSearchSessionsBySourceService{
     override fun findAllSessionsBySource (
         presenter: FindAllSearchSessionsBySourcePresenter, request: RequestModel
     ) {
-        val researcherId = ResearcherId(request.researcherId)
-        val systematicStudyId = SystematicStudyId(request.systematicStudyId)
-        val preconditionChecker = PreconditionChecker(systematicStudyRepository, credentialsService)
-        preconditionChecker.prepareIfViolatesPreconditions(presenter, researcherId, systematicStudyId)
+        val user = credentialsService.loadCredentials(request.userId)?.toUser()
+
+        val systematicStudyDto = systematicStudyRepository.findById(request.systematicStudyId)
+        val systematicStudy = systematicStudyDto?.let { SystematicStudy.fromDto(it) }
+
+        presenter.prepareIfFailsPreconditions(user, systematicStudy)
 
         if(presenter.isDone()) return
 
@@ -28,7 +34,7 @@ class FindAllSearchSessionsBySourceServiceImpl(
             request.systematicStudyId, request.source
         )
         presenter.prepareSuccessView(ResponseModel(
-            request.researcherId, request.systematicStudyId, request.source, searchSessions
+            request.userId, request.systematicStudyId, request.source, searchSessions
         ))
     }
 }
