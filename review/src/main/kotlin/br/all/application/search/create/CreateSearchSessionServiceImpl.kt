712722c08cadfdcf2d4e2bf1c2eca1a60e596be2
SauloFernando99 <saulofernando114@gmail.com>
2024-05-18 20:10:05 -0300

    refactor: changed the old credential service for the newer

--- review/src/main/kotlin/br/all/application/search/create/CreateSearchSessionServiceImpl.kt
+++ review/src/main/kotlin/br/all/application/search/create/CreateSearchSessionServiceImpl.kt
@@ -2,6 +2,7 @@ package br.all.application.search
 
 import br.all.application.user.credentials.ResearcherCredentialsService
 import br.all.application.review.repository.SystematicStudyRepository
+import br.all.application.review.repository.fromDto
 import br.all.application.search.create.CreateSearchSessionPresenter
 import br.all.application.search.create.CreateSearchSessionService
 import br.all.application.search.create.CreateSearchSessionService.RequestModel
@@ -11,9 +12,13 @@ import br.all.application.search.repository.fromRequestModel
 import br.all.application.search.repository.toDto
 import br.all.application.shared.exceptions.UniquenessViolationException
 import br.all.application.shared.presenter.PreconditionChecker
+import br.all.application.shared.presenter.prepareIfFailsPreconditions
 import br.all.application.study.repository.StudyReviewRepository
 import br.all.application.study.repository.toDto
+import br.all.application.user.CredentialsService
 import br.all.domain.model.researcher.toResearcherId
+import br.all.domain.model.review.SystematicStudy
+import br.all.domain.model.review.SystematicStudyId
 import br.all.domain.model.review.toSystematicStudyId
 import br.all.domain.model.search.SearchSession
 import br.all.domain.model.search.SearchSessionID
@@ -26,25 +31,27 @@ class CreateSearchSessionServiceImpl(
     private val uuidGeneratorService: UuidGeneratorService,
     private val bibtexConverterService: BibtexConverterService,
     private val studyReviewRepository: StudyReviewRepository,
-    private val credentialsService: ResearcherCredentialsService,
+    private val credentialsService: CredentialsService,
 ) : CreateSearchSessionService {
 
 
     override fun createSession(presenter: CreateSearchSessionPresenter, request: RequestModel, file: String) {
-        val (researcher, systematicStudy) = request
 
-        PreconditionChecker(systematicStudyRepository, credentialsService).also {
-            it.prepareIfViolatesPreconditions(presenter, researcher.toResearcherId(), systematicStudy.toSystematicStudyId())
-        }
+        val user = credentialsService.loadCredentials(request.userId)?.toUser()
+
+        val systematicStudyDto = systematicStudyRepository.findById(request.systematicStudyId)
+        val systematicStudy = systematicStudyDto?.let { SystematicStudy.fromDto(it) }
+        presenter.prepareIfFailsPreconditions(user, systematicStudy)
+
         if (presenter.isDone()) return
 
         val sessionId = SearchSessionID(uuidGeneratorService.next())
         val searchSession = SearchSession.fromRequestModel(sessionId, request)
 
-        val studyReviews = bibtexConverterService.convertManyToStudyReview(systematicStudy.toSystematicStudyId(), file)
+        val studyReviews = bibtexConverterService.convertManyToStudyReview(request.systematicStudyId.toSystematicStudyId() , file)
         studyReviewRepository.saveOrUpdateBatch(studyReviews.map { it.toDto() })
 
         searchSessionRepository.create(searchSession.toDto())
-        presenter.prepareSuccessView(ResponseModel(researcher, systematicStudy, sessionId.value))
+        presenter.prepareSuccessView(ResponseModel(request.userId, request.systematicStudyId, sessionId.value))
     }
 }
