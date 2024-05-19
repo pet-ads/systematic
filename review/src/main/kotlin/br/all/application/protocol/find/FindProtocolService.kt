678081843d2f02f4b88136b8a2d55b8a31c1f329
gabrmanh <gmanhani@yahoo.com>
2024-05-18 21:54:19 -0300

    refactor: changed protocol services to new standard

--- review/src/main/kotlin/br/all/application/protocol/find/FindProtocolService.kt
+++ review/src/main/kotlin/br/all/application/protocol/find/FindProtocolService.kt
@@ -7,12 +7,12 @@ interface FindProtocolService {
     fun findById(presenter: FindProtocolPresenter, request: RequestModel)
 
     data class RequestModel(
-        val researcherId: UUID,
+        val userId: UUID,
         val systematicStudyId: UUID,
     )
 
     data class ResponseModel(
-        val researcherId: UUID,
+        val userId: UUID,
         val systematicStudyId: UUID,
         val content: ProtocolDto,
     )
