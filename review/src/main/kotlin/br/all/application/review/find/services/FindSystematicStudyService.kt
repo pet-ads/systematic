764c0575f7e15a8b10d96bcbd4ad67e0e19d2fe0
gabrmanh <gmanhani@yahoo.com>
2024-05-20 20:02:54 -0300

    refactor: change systematic study services to new account logic

--- review/src/main/kotlin/br/all/application/review/find/services/FindSystematicStudyService.kt
+++ review/src/main/kotlin/br/all/application/review/find/services/FindSystematicStudyService.kt
@@ -8,12 +8,12 @@ interface FindSystematicStudyService {
     fun findById(presenter: FindSystematicStudyPresenter, request: RequestModel)
 
     data class RequestModel(
-        val researcher: UUID,
+        val userId: UUID,
         val systematicStudy: UUID,
     )
 
     data class ResponseModel(
-        val researcherId: UUID,
+        val userId: UUID,
         val systematicStudyId: UUID,
         val content: SystematicStudyDto,
     )
