b9f221bb1807ea5318f7c9ededd4290f317b3512
gabrmanh <gmanhani@yahoo.com>
2024-05-26 23:01:58 -0300

    test: create lambda function to test for unauthorized and unauthenticated users

--- web/src/test/kotlin/br/all/shared/TestHelperService.kt
+++ web/src/test/kotlin/br/all/shared/TestHelperService.kt
@@ -4,12 +4,21 @@ import br.all.application.user.repository.UserAccountDto
 import br.all.application.user.repository.UserAccountRepository
 import br.all.security.service.ApplicationUser
 import io.github.serpro69.kfaker.Faker
+import org.springframework.http.MediaType
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors
 import org.springframework.stereotype.Service
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.time.LocalDateTime
 import java.util.*
 
 @Service
-class TestHelperService(private val repository: UserAccountRepository) {
+class TestHelperService(
+    private val repository: UserAccountRepository,
+    private val mockMvc: MockMvc
+) {
 
     private val faker = Faker()
 
@@ -55,7 +64,30 @@ class TestHelperService(private val repository: UserAccountRepository) {
         return applicationUser
     }
 
+    fun testForUnauthorizedUser(requestBuilder: MockHttpServletRequestBuilder, action: MockMvcAction) {
+        val unauthorizedUser = createUnauthorizedApplicationUser()
+
+        val request = requestBuilder
+            .with(SecurityMockMvcRequestPostProcessors.user(unauthorizedUser))
+            .contentType(MediaType.APPLICATION_JSON)
+
+        mockMvc.action(request)
+            .andExpect(status().isForbidden)
+
+        deleteApplicationUser(unauthorizedUser.id)
+    }
+
+    fun testForUnauthenticatedUser(requestBuilder: MockHttpServletRequestBuilder, action: MockMvcAction) {
+        val request = requestBuilder
+            .contentType(MediaType.APPLICATION_JSON)
+
+        mockMvc.action(request)
+            .andExpect(status().isUnauthorized)
+    }
+
     fun deleteApplicationUser(id: UUID) {
         repository.deleteById(id)
     }
 }
+
+typealias MockMvcAction = MockMvc.(MockHttpServletRequestBuilder) -> ResultActions
