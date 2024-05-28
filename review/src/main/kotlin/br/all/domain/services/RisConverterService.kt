74558f078937c911db44a44c4a3aebf52df4ecaa
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-05-28 19:51:57 -0300

    chore: first commit.

---
+++ review/src/main/kotlin/br/all/domain/services/RisConverterService.kt
@@ -0,0 +1,9 @@
+package br.all.domain.services
+
+import br.all.domain.model.review.SystematicStudyId
+import br.all.domain.model.study.*
+import java.util.*
+
+class RisConverterService(private val studyReviewIdGeneratorService: IdGeneratorService) {
+
+}
