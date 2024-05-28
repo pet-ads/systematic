d352592f681847e42d4a8640fa3809a1ecf29e2a
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-05-28 19:54:23 -0300

    feat: add method to convert RIS to StudyReview.

--- review/src/main/kotlin/br/all/domain/services/RisConverterService.kt
+++ review/src/main/kotlin/br/all/domain/services/RisConverterService.kt
@@ -5,5 +5,32 @@ import br.all.domain.model.study.*
 import java.util.*
 
 class RisConverterService(private val studyReviewIdGeneratorService: IdGeneratorService) {
+    fun convertToStudyReview(systematicStudyId: SystematicStudyId, ris: String): StudyReview {
+        require(ris.isNotBlank()) { "RIS must not be blank." }
 
+        val studyReviewId = StudyReviewId(studyReviewIdGeneratorService.next())
+        val study = convert(ris)
+
+        return StudyReview(
+            studyReviewId,
+            systematicStudyId,
+            study.type,
+            study.title,
+            study.year,
+            study.authors,
+            study.venue,
+            study.abstract,
+            study.doi,
+            study.keywords,
+            mutableSetOf("insert SearchSources"),
+            study.references,
+            mutableSetOf(),
+            mutableSetOf(),
+            mutableSetOf(),
+            "",
+            ReadingPriority.LOW,
+            SelectionStatus.UNCLASSIFIED,
+            ExtractionStatus.UNCLASSIFIED
+        )
+    }
 }
