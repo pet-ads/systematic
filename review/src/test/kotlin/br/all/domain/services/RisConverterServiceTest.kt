6e21def66df91bc7b2b49833da3b16d3873c268f
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-17 16:36:01 -0300

    test: create beforeEach and teardown.

--- review/src/test/kotlin/br/all/domain/services/RisConverterServiceTest.kt
+++ review/src/test/kotlin/br/all/domain/services/RisConverterServiceTest.kt
@@ -1,4 +1,30 @@
 package br.all.domain.services
 
+import br.all.domain.model.review.SystematicStudyId
+import br.all.domain.model.study.*
+import br.all.domain.services.BibtexTestData.testInputs
+import br.all.domain.services.RisTestData.testInput
+import org.junit.jupiter.api.*
+import java.util.*
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
 class RisConverterServiceTest {
+    private lateinit var sut: RisConverterService
+    private lateinit var idGeneratorService: IdGeneratorService
+
+    @BeforeEach
+    fun setup() {
+        val systematicStudyId = SystematicStudyId(UUID.randomUUID())
+        idGeneratorService = FakeIdGeneratorService
+        sut = RisConverterService(idGeneratorService)
+    }
+
+    @AfterEach
+    fun teardown() {
+        val fake = idGeneratorService as FakeIdGeneratorService
+        fake.reset()
+    }
+
 }
