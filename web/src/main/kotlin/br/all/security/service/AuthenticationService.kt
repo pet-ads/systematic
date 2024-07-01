a161bd9e62fd6834e788d800f03e4b01da57dcb5
SauloFernando99 <saulofernando114@gmail.com>
2024-07-01 11:09:00 -0300

    feat: included hateoas to return links for findAllSystematicStudiesByOwner and postSystematicStudy

--- web/src/main/kotlin/br/all/security/service/AuthenticationService.kt
+++ web/src/main/kotlin/br/all/security/service/AuthenticationService.kt
@@ -3,9 +3,13 @@ package br.all.security.service
 import br.all.application.user.find.LoadAccountCredentialsService
 import br.all.application.user.update.UpdateRefreshTokenService
 import br.all.application.user.update.UpdateRefreshTokenService.RequestModel
+import br.all.review.controller.SystematicStudyController
+import br.all.review.requests.PostRequest
 import br.all.security.auth.AuthenticationRequest
 import br.all.security.auth.AuthenticationResponse
 import br.all.security.config.JwtProperties
+import org.springframework.hateoas.RepresentationModel
+import org.springframework.hateoas.server.mvc.linkTo
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -21,7 +25,7 @@ class AuthenticationService(
     private val loadCredentialsService: LoadAccountCredentialsService,
     private val updateRefreshTokenService: UpdateRefreshTokenService
 ) {
-    fun authenticate(request: AuthenticationRequest): AuthenticationResponse {
+    fun authenticate(request: AuthenticationRequest): AuthenticationResponseModel {
         authManager.authenticate(UsernamePasswordAuthenticationToken(request.username, request.password))
         val user = userDetailService.loadUserByUsername(request.username) as ApplicationUser
 
@@ -31,8 +35,22 @@ class AuthenticationService(
         val  updateTokenRequest = RequestModel(user.id, refreshToken)
         updateRefreshTokenService.update(updateTokenRequest)
 
-        return AuthenticationResponse(token, refreshToken)
+        val responseModel = AuthenticationResponseModel(token, refreshToken)
+        val ownerStudies = linkToFindAllByOwner(user.id)
+        val createSystematicStudy = linkToPostSystematicStudy()
+
+        responseModel.add(ownerStudies, createSystematicStudy)
+
+        return responseModel
     }
+
+    private fun linkToFindAllByOwner(ownerId: UUID) = linkTo<SystematicStudyController> {
+        findAllSystematicStudiesByOwner(ownerId)
+    }.withRel("my-reviews")
+
+    private fun linkToPostSystematicStudy() = linkTo<SystematicStudyController> {
+        postSystematicStudy(PostRequest("title", "description", setOf(UUID.randomUUID())))
+    }.withRel("create-review")
 
     private fun generateToken(user: ApplicationUser, duration: Long) = tokenService.generateToken(
         user,
@@ -55,4 +73,9 @@ class AuthenticationService(
         currentUserId: UUID,
         refreshTokenUserId: UUID?
     ) = tokenService.isExpired(refreshToken) || currentUserId != refreshTokenUserId
+
+    data class AuthenticationResponseModel(
+        val accessToken: String,
+        val refreshToken: String
+    ): RepresentationModel<AuthenticationResponseModel>()
 }
