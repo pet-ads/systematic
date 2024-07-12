bc91258bb2ee770158dac43ecb8ea4e74d425e4f
SauloFernando99 <saulofernando114@gmail.com>
2024-07-12 19:47:09 -0300

    docs(PutRequest): marked PutRequest as Schema for swagger

--- web/src/main/kotlin/br/all/review/requests/PutRequest.kt
+++ web/src/main/kotlin/br/all/review/requests/PutRequest.kt
@@ -1,8 +1,10 @@
 package br.all.review.requests
 
 import br.all.application.review.update.services.UpdateSystematicStudyService
+import io.swagger.v3.oas.annotations.media.Schema
 import java.util.*
 
+@Schema(name = "SystematicStudyPutRequest")
 data class PutRequest(val title: String?, val description: String?) {
     fun toUpdateRequestModel(researcherId: UUID, systematicStudyId: UUID) =
         UpdateSystematicStudyService.RequestModel(researcherId, systematicStudyId, title, description)
