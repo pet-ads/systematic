ee418b072677258fd66d0fe01d6233ec32639161
SauloFernando99 <saulofernando114@gmail.com>
2024-08-26 20:20:52 -0300

    feat: created a put request to be used by the update question request

--- web/src/main/kotlin/br/all/question/requests/PutRequest.kt
+++ web/src/main/kotlin/br/all/question/requests/PutRequest.kt
@@ -1,4 +1,31 @@
 package br.all.question.requests
 
-class PutRequest {
+import br.all.application.question.create.CreateQuestionService
+import br.all.application.question.update.services.UpdateQuestionService
+import io.swagger.v3.oas.annotations.media.Schema
+import java.util.*
+
+@Schema(name = "UpdateQuestionPutRequest")
+data class PutRequest (
+    val questionType: CreateQuestionService.QuestionType,
+    val code: String,
+    val description: String,
+    val scales: Map<String, Int>? = null,
+    val higher: Int? = null,
+    val lower: Int? = null,
+    val options: List<String>? = null,
+){
+    fun toUpdateRequestModel(userId: UUID, systematicStudyId: UUID, questionId: UUID) =
+        UpdateQuestionService.RequestModel(
+            userId,
+            systematicStudyId,
+            questionId,
+            questionType,
+            code,
+            description,
+            scales,
+            higher,
+            lower,
+            options
+        )
 }
