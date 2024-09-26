b52a4abfbf7404f74a60d485856c0ae15004daeb
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-09-26 14:26:12 -0300

    feat: create search session service implementation

---
+++ review/src/main/kotlin/br/all/application/search/update/PatchSearchSessionServiceImpl.kt
@@ -0,0 +1,51 @@
+package br.all.application.search.update
+
+import br.all.application.review.repository.SystematicStudyRepository
+import br.all.application.review.repository.fromDto
+import br.all.application.search.repository.SearchSessionRepository
+import br.all.application.search.repository.fromDto
+import br.all.application.search.repository.toDto
+import br.all.application.search.update.UpdateSearchSessionService.RequestModel
+import br.all.application.search.update.UpdateSearchSessionService.ResponseModel
+import br.all.application.shared.exceptions.EntityNotFoundException
+import br.all.application.shared.presenter.prepareIfFailsPreconditions
+import br.all.application.user.CredentialsService
+import br.all.domain.model.protocol.toSearchSource
+import br.all.domain.model.review.SystematicStudy
+import br.all.domain.model.search.SearchSession
+
+class PatchSearchSessionServiceImpl (
+    private val systematicStudyRepository: SystematicStudyRepository,
+    private val searchSessionRepository: SearchSessionRepository,
+    private val credentialsService: CredentialsService,
+) : UpdateSearchSessionService {
+    override fun updateSession(presenter: UpdateSearchSessionPresenter, request: RequestModel
+    ) {
+        val user = credentialsService.loadCredentials(request.userId)?.toUser()
+
+        val systematicStudyDto = systematicStudyRepository.findById(request.systematicStudyId)
+        val systematicStudy = systematicStudyDto?.let { SystematicStudy.fromDto(it) }
+
+        presenter.prepareIfFailsPreconditions(user, systematicStudy)
+
+
+        if(presenter.isDone()) return
+
+        if (searchSessionRepository.existsById(request.sessionId)) {
+            val dto = searchSessionRepository.findById(request.sessionId) ?: return
+            val patch = SearchSession.fromDto(dto).apply {
+                searchString += request.searchString ?: searchString
+                additionalInfo += request.additionalInfo ?: additionalInfo
+                source = (source.toString() + request.source).toSearchSource()
+            }.toDto()
+
+            if (patch != dto) searchSessionRepository.saveOrUpdate(patch)
+
+            presenter.prepareSuccessView(ResponseModel(request.userId, request.systematicStudyId, request.sessionId))
+
+        } else {
+            val message = "There is no search session of id ${request.sessionId}"
+            presenter.prepareFailView(EntityNotFoundException(message))
+        }
+    }
+}
