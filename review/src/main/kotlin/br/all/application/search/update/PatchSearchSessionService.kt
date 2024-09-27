a7658ca7bf13268dd66d50532de2d7b0a66149db
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-09-27 14:05:47 -0300

    refactor: remove unused request add file to patchSession fun

--- review/src/main/kotlin/br/all/application/search/update/PatchSearchSessionService.kt
+++ review/src/main/kotlin/br/all/application/search/update/PatchSearchSessionService.kt
@@ -4,15 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema
 import java.util.*
 
 interface PatchSearchSessionService {
-    fun patchSession(presenter: PatchSearchSessionPresenter, request: RequestModel)
+    fun patchSession(presenter: PatchSearchSessionPresenter, request: RequestModel, file: String)
 
     data class RequestModel(
         val userId: UUID,
         val systematicStudyId: UUID,
         val sessionId: UUID,
-        val searchString: String?,
-        val additionalInfo: String?,
-        val source: String?
     )
 
     @Schema(name = "PatchSearchSessionServiceResponseModel")
