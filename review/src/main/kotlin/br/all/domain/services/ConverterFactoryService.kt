19da2c42f6f5aec2632ab166ab7d19cd385daf3e
SauloFernando99 <saulofernando114@gmail.com>
2024-09-30 15:44:19 -0300

    fix: fixed the params for the converters services

--- review/src/main/kotlin/br/all/domain/services/ConverterFactoryService.kt
+++ review/src/main/kotlin/br/all/domain/services/ConverterFactoryService.kt
@@ -2,6 +2,7 @@ package br.all.domain.services
 
 
 import br.all.domain.model.review.SystematicStudyId
+import br.all.domain.model.search.SearchSessionID
 import br.all.domain.model.study.StudyReview
 import java.util.*
 
@@ -11,7 +12,10 @@ class ConverterFactoryService(
     private val risConverterService: RisConverterService
 ){
     // , request: RequestModel
-    fun extractReferences(file: String): List<StudyReview> {
+    fun extractReferences(
+        systematicStudyId: SystematicStudyId,
+        searchSessionId: SearchSessionID,
+        file: String): List<StudyReview> {
         var studyReviews = listOf<StudyReview>()
 
         val bibtexPattern = Regex("""@\s*(article|book|conference|inproceedings|techreport|phdthesis|mastersthesis)\s*""", RegexOption.IGNORE_CASE)
@@ -19,9 +23,9 @@ class ConverterFactoryService(
         val risPattern = Regex("""^TY\s+-\s+""", RegexOption.MULTILINE)
 
         if (bibtexPattern.containsMatchIn(file)) {
-            studyReviews = bibtexConverterService.convertManyToStudyReview(SystematicStudyId(UUID.randomUUID()), file)
+            studyReviews = bibtexConverterService.convertManyToStudyReview(systematicStudyId, searchSessionId, file)
         } else if (risPattern.containsMatchIn(file)) {
-            studyReviews = risConverterService.convertManyToStudyReview(SystematicStudyId(UUID.randomUUID()), file)
+            studyReviews = risConverterService.convertManyToStudyReview(systematicStudyId, searchSessionId, file)
         }
 
         return studyReviews
