db0a7bbcef6f9929d3b3c49470381d411a356f80
ericksgmes <erick.dicksan@gmail.com>
2024-11-28 11:33:43 -0300

    refactor: change factory service to return pair

--- review/src/main/kotlin/br/all/domain/services/ConverterFactoryService.kt
+++ review/src/main/kotlin/br/all/domain/services/ConverterFactoryService.kt
@@ -4,30 +4,29 @@ package br.all.domain.services
 import br.all.domain.model.review.SystematicStudyId
 import br.all.domain.model.search.SearchSessionID
 import br.all.domain.model.study.StudyReview
-import java.util.*
-
 
 class ConverterFactoryService(
     private val bibtexConverterService: BibtexConverterService,
     private val risConverterService: RisConverterService
-){
+) {
     fun extractReferences(
         systematicStudyId: SystematicStudyId,
         searchSessionId: SearchSessionID,
-        file: String): List<StudyReview> {
-        var studyReviews = listOf<StudyReview>()
-
+        file: String
+    ): Pair<List<StudyReview>, List<String>> {
         val bibtexPattern = Regex("""@\s*(article|book|conference|inproceedings|techreport|phdthesis|mastersthesis)\s*""", RegexOption.IGNORE_CASE)
-
         val risPattern = Regex("""^TY\s+-\s+""", RegexOption.MULTILINE)
 
-        if (bibtexPattern.containsMatchIn(file)) {
-            studyReviews = bibtexConverterService.convertManyToStudyReview(systematicStudyId, searchSessionId, file).first
-        } else if (risPattern.containsMatchIn(file)) {
-            studyReviews = risConverterService.convertManyToStudyReview(systematicStudyId, searchSessionId, file).first
+        return when {
+            bibtexPattern.containsMatchIn(file) -> {
+                bibtexConverterService.convertManyToStudyReview(systematicStudyId, searchSessionId, file)
+            }
+            risPattern.containsMatchIn(file) -> {
+                risConverterService.convertManyToStudyReview(systematicStudyId, searchSessionId, file)
+            }
+            else -> {
+                Pair(emptyList(), listOf("File format not recognized or unsupported"))
+            }
         }
-
-        return studyReviews
     }
-
 }
