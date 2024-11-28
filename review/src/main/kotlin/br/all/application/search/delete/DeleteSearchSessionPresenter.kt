3f3e5935420241cc2e40bce360c5f1e5ddd75bf2
ericksgmes <erick.dicksan@gmail.com>
2024-11-28 19:06:36 -0300

    feat: implement RestfulDeleteSearchSessionPresenter for handling HTTP responses in delete operations

---
+++ review/src/main/kotlin/br/all/application/search/delete/DeleteSearchSessionPresenter.kt
@@ -0,0 +1,6 @@
+package br.all.application.search.delete
+
+import br.all.application.shared.presenter.GenericPresenter
+import br.all.application.search.delete.DeleteSearchSessionService.ResponseModel
+
+interface DeleteSearchSessionPresenter: GenericPresenter<ResponseModel>
