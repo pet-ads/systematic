803d7d4cd1bd0088c70a55d495a0ab9ded2f3c6c
ericksgmes <erick.dicksan@gmail.com>
2025-02-10 12:17:42 -0300

    feat: add context parameter to QuestionDto generation methods

--- review/src/main/kotlin/br/all/application/question/util/TestDataFactory.kt
+++ review/src/main/kotlin/br/all/application/question/util/TestDataFactory.kt
@@ -20,76 +20,82 @@ class TestDataFactory {
         systematicStudyId: UUID = systematicStudy,
         code: String = faker.lorem.words(),
         description: String = faker.lorem.words(),
-    ) =
-        QuestionDto(
-            questionId,
-            systematicStudyId,
-            code,
-            description,
-            "TEXTUAL",
-            null,
-            null,
-            null,
-            null
-        )
+        context: QuestionContext = QuestionContext.EXTRACTION // <-- Tipo ajustado
+    ) = QuestionDto(
+        questionId,
+        systematicStudyId,
+        code,
+        description,
+        "TEXTUAL",
+        null,
+        null,
+        null,
+        null,
+        context.toString()
+    )
 
     fun generatePickListDto(
         questionId: UUID = question,
         systematicStudyId: UUID = systematicStudy,
-    ) =
-        QuestionDto(
-            questionId,
-            systematicStudyId,
-            code,
-            description,
-            "PICK_LIST",
-            null,
-            null,
-            null,
-            listOf(faker.lorem.words(), faker.lorem.words())
-        )
+        context: QuestionContext = QuestionContext.EXTRACTION
+    ) = QuestionDto(
+        questionId,
+        systematicStudyId,
+        code,
+        description,
+        "PICK_LIST",
+        null,
+        null,
+        null,
+        listOf(faker.lorem.words(), faker.lorem.words()),
+        context.toString()
+    )
 
     fun generateLabeledScaleDto(
         questionId: UUID = question,
         systematicStudyId: UUID = systematicStudy,
-    ) =
-        QuestionDto(
-            questionId,
-            systematicStudyId,
-            code,
-            description,
-            "LABELED_SCALE",
-            mapOf(faker.lorem.words() to 1, faker.lorem.words() to 2),
-            null,
-            null,
-            null
-        )
+        context: QuestionContext = QuestionContext.EXTRACTION
+    ) = QuestionDto(
+        questionId,
+        systematicStudyId,
+        code,
+        description,
+        "LABELED_SCALE",
+        mapOf(faker.lorem.words() to 1, faker.lorem.words() to 2),
+        null,
+        null,
+        null,
+        context.toString()
+    )
 
     fun generateNumberedScaleDto(
         questionId: UUID = question,
         systematicStudyId: UUID = systematicStudy,
-    ) =
-        QuestionDto(
-            questionId,
-            systematicStudyId,
-            code,
-            description,
-            "NUMBERED_SCALE",
-            null,
-            1,
-            10,
-            null,
-        )
+        context: QuestionContext = QuestionContext.EXTRACTION
+    ) = QuestionDto(
+        questionId,
+        systematicStudyId,
+        code,
+        description,
+        "NUMBERED_SCALE",
+        null,
+        1,
+        10,
+        null,
+        context.toString()
+    )
 
     fun updateQuestionRequestModel(
         updatedDto: QuestionDto,
         questionType: QuestionType,
         researcherId: UUID = researcher,
+        context: QuestionContext = QuestionContext.EXTRACTION
     ) = UpdateQuestionService.RequestModel(
         researcherId,
         updatedDto.systematicStudyId,
         updatedDto.questionId,
         questionType,
+        context,
         updatedDto.code,
         updatedDto.description,
         updatedDto.scales,
@@ -102,9 +108,11 @@ class TestDataFactory {
         researcherId: UUID = researcher,
         systematicStudyId: UUID = systematicStudy,
         questionType: QuestionType = QuestionType.TEXTUAL,
+        context: QuestionContext = QuestionContext.EXTRACTION
     ) = RequestModel(
         researcherId,
         systematicStudyId,
+        context,
         questionType,
         code,
         description,
@@ -118,10 +126,12 @@ class TestDataFactory {
         researcherId: UUID = researcher,
         systematicStudyId: UUID = systematicStudy,
         questionType: QuestionType = QuestionType.PICK_LIST,
-        options: List<String>? = listOf(faker.lorem.words(), faker.lorem.words())
+        options: List<String>? = listOf(faker.lorem.words(), faker.lorem.words()),
+        context: QuestionContext = QuestionContext.EXTRACTION
     ) = RequestModel(
         researcherId,
         systematicStudyId,
+        context,
         questionType,
         code,
         description,
@@ -136,9 +146,11 @@ class TestDataFactory {
         systematicStudyId: UUID = systematicStudy,
         questionType: QuestionType = QuestionType.LABELED_SCALE,
         scales: Map<String, Int>? = mapOf(faker.lorem.words() to 1, faker.lorem.words() to 2),
+        context: QuestionContext = QuestionContext.EXTRACTION
     ) = RequestModel(
         researcherId,
         systematicStudyId,
+        context,
         questionType,
         code,
         description,
@@ -153,10 +165,12 @@ class TestDataFactory {
         systematicStudyId: UUID = systematicStudy,
         questionType: QuestionType = QuestionType.NUMBERED_SCALE,
         higher: Int? = 10,
-        lower: Int? = 1
+        lower: Int? = 1,
+        context: QuestionContext = QuestionContext.EXTRACTION
     ) = RequestModel(
         researcherId,
         systematicStudyId,
+        context,
         questionType,
         code,
         description,
@@ -204,7 +218,7 @@ class TestDataFactory {
     fun dtoFromRequest(
         request: RequestModel,
         questionId: UUID = question,
-        systematicStudyId: UUID = systematicStudy,
+        systematicStudyId: UUID = systematicStudy
     ) = QuestionDto(
         questionId,
         systematicStudyId,
@@ -214,7 +228,8 @@ class TestDataFactory {
         request.scales,
         request.lower,
         request.higher,
-        request.options
+        request.options,
+        request.questionContext.toString()
     )
 
     operator fun component1() = researcher
