fb7dd9cd72aea1c1d7dcd76cbbd3270f29314897
gabrmanh <gmanhani@yahoo.com>
2025-02-21 23:32:31 -0300

    fix: move enum valueOf usage to service layer to correct improper dependency

--- review/src/main/kotlin/br/all/application/question/repository/QuestionRepository.kt
+++ review/src/main/kotlin/br/all/application/question/repository/QuestionRepository.kt
@@ -1,9 +1,10 @@
 package br.all.application.question.repository
 
+import br.all.domain.model.question.QuestionContextEnum
 import java.util.UUID
 
 interface QuestionRepository {
     fun createOrUpdate(dto: QuestionDto)
     fun findById(systematicStudyId: UUID, id: UUID): QuestionDto?
-    fun findAllBySystematicStudyId(systematicStudyId: UUID, context: String? = null): List<QuestionDto>
+    fun findAllBySystematicStudyId(systematicStudyId: UUID, context: QuestionContextEnum? = null): List<QuestionDto>
 }
