f7de9bf64fcd1b0a656cab1e0d1e0b5df7c2e6d7
ericksgmes <erick.dicksan@gmail.com>
2025-02-27 12:55:14 -0300

    refactor: creating more questions of more type, also creating rob questions

--- web/src/main/kotlin/br/all/WebApplication.kt
+++ web/src/main/kotlin/br/all/WebApplication.kt
@@ -35,47 +35,103 @@ class WebApplication {
         //TODO Insira as questões antes do protocolo (talvez dentro da classe CreateSystematicReviewExampleService, assim você vai ter os UUIDs para usar.
         //TODO Todas as questões aqui são extraction. Se precisar, invente uma para ROB, só para voltar na UI.
 
-        val rq1 = question.createQuestion(
+        val eq1 = question.createQuestion(
             reviewId = systematicId,
-            code = "RQ1",
+            code = "EQ1",
             title = "How has service-orientation been applied to the development of robotic systems?",
+            type = "TEXTUAL",
             context = QuestionContextEnum.EXTRACTION
         )
 
-        val rq2 = question.createQuestion(
+        val eq2 = question.createQuestion(
             reviewId = systematicId,
-            code = "RQ2",
+            code = "EQ2",
             title = "What is the most common way of interaction among service-oriented robotic systems?",
-            context = QuestionContextEnum.EXTRACTION
+            type = "NUMBERED_SCALE",
+            context = QuestionContextEnum.EXTRACTION,
+            higher = 5,
+            lower = 1
         )
 
-        val rq3 = question.createQuestion(
+        val eq3 = question.createQuestion(
             reviewId = systematicId,
-            code = "RQ3",
+            code = "EQ3",
             title = "What implementation technology has been mostly used to develop service-oriented robotic systems?",
-            context = QuestionContextEnum.EXTRACTION
+            type = "LABELED_SCALE",
+            context = QuestionContextEnum.EXTRACTION,
+            scales = mapOf(
+                "How clearly are the review’s research objectives stated?" to 1,
+                "How comprehensive is the search strategy (databases, keywords, and search strings)?" to 2,
+                "How transparent is the data extraction and synthesis process?" to 3,
+                "How well are the findings (tables, graphs, mapping) organized and reported?" to 4,
+                "How adequately are the threats to validity discussed?" to 5
+            )
         )
 
-        val rq4 = question.createQuestion(
+        val eq4 = question.createQuestion(
             reviewId = systematicId,
-            code = "RQ4",
-            title = "What are the development environments and tools that support the development of service-oriented robotic systems?",
-            context = QuestionContextEnum.EXTRACTION
+            code = "EQ4",
+            title = "Which research question provides the most valuable insight for future research?",
+            type = "PICK_LIST",
+            context = QuestionContextEnum.EXTRACTION,
+            options = listOf(
+                "RQ1: How has service-orientation been applied to the development of robotic systems?",
+                "RQ2: What is the most common way of interaction among service-oriented robotic systems?",
+                "RQ3: What implementation technology has been mostly used to develop service-oriented robotic systems?",
+                "RQ4: What are the development environments and tools that support the development of service-oriented robotic systems?",
+                "RQ5: Is SOA applicable to all types of robots?",
+                "RQ6: How has Software Engineering been applied to the development of service-oriented robotic systems?"
+            )
         )
 
-        val rq5 = question.createQuestion(
+        val rbq1 = question.createQuestion(
             reviewId = systematicId,
-            code = "RQ56",
-            title = "Is SOA applicable to all types of robots?",
-            context = QuestionContextEnum.EXTRACTION
+            code = "RBQ1",
+            title = "In your own words, describe potential sources of selection bias in this review.",
+            type = "TEXTUAL",
+            context = QuestionContextEnum.ROB
         )
 
-        val rq6 = question.createQuestion(
+        val rbq2 = question.createQuestion(
             reviewId = systematicId,
-            code = "RQ6",
-            title = "How has Software Engineering been applied to the development of service-oriented robotic systems?",
-            context = QuestionContextEnum.EXTRACTION
+            code = "RBQ2",
+            title = "Which option best describes the overall risk of bias in the review?",
+            type = "PICK_LIST",
+            context = QuestionContextEnum.ROB,
+            options = listOf(
+                "Very High",
+                "High",
+                "Moderate",
+                "Low",
+                "Very Low"
+            )
         )
+
+        val rbq3 = question.createQuestion(
+            reviewId = systematicId,
+            code = "RBQ3",
+            title = "How effectively is publication bias minimized (e.g., through multiple databases and specialist suggestions)?",
+            type = "LABELED_SCALE",
+            context = QuestionContextEnum.ROB,
+            scales = mapOf(
+                "Not effective at all" to 1,
+                "Slightly effective" to 2,
+                "Moderately effective" to 3,
+                "Very effective" to 4,
+                "Extremely effective" to 5
+            )
+        )
+
+        val rbq4 = question.createQuestion(
+            reviewId = systematicId,
+            code = "RBQ4",
+            title = "Rate the overall reliability of the review process from 1 (very unreliable) to 5 (very reliable).",
+            type = "NUMBERED_SCALE",
+            context = QuestionContextEnum.ROB,
+            higher = 5,
+            lower = 1
+        )
+
 
         //TODO o arquivo ALL.bib está na pasta resources. Ao invés de usar um só, insira um para cada base, criando
         //TODO search sessions realistas (com a string adaptada, campo de observação, data da busca, etc.)
