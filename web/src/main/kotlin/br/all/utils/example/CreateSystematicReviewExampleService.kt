a15a67716c044de856a6d2ff3ddead5fa7f25238
ericksgmes <erick.dicksan@gmail.com>
2025-02-27 13:00:47 -0300

    refactor: questions are now created in CreateSystematicReviewExampleService before the creation of the protocol

--- web/src/main/kotlin/br/all/utils/example/CreateSystematicReviewExampleService.kt
+++ web/src/main/kotlin/br/all/utils/example/CreateSystematicReviewExampleService.kt
@@ -6,6 +6,7 @@ import br.all.application.review.repository.SystematicStudyDto
 import br.all.application.review.repository.SystematicStudyRepository
 import br.all.application.protocol.repository.ProtocolDto
 import br.all.application.protocol.repository.ProtocolRepository
+import br.all.domain.model.question.QuestionContextEnum
 import br.all.domain.services.UuidGeneratorService
 import org.springframework.stereotype.Service
 import java.util.UUID
@@ -15,14 +16,15 @@ class CreateSystematicReviewExampleService(
     private val uuidGeneratorService: UuidGeneratorService,
     private val systematicStudyRepository: SystematicStudyRepository,
     private val protocolRepository: ProtocolRepository,
+    private val question: CreateQuestionExampleService
 ) {
     fun createReview(ownerId: UUID, collaboratorIds: Set<UUID>): UUID {
 
-        val systematicStudyId = uuidGeneratorService.next()
+        val systematicId = uuidGeneratorService.next()
 
         systematicStudyRepository.saveOrUpdate(
             SystematicStudyDto(
-                id = systematicStudyId,
+                id = systematicId,
                 title = "A Systematic Review on Service-Oriented Robotic Systems Development",
                 description = """
                 This systematic review, conducted in January 2012 at ICMC/USP, presents a detailed panorama on 
@@ -34,10 +36,107 @@ class CreateSystematicReviewExampleService(
             )
         )
 
+        val eq1 = question.createQuestion(
+            reviewId = systematicId,
+            code = "EQ1",
+            title = "How has service-orientation been applied to the development of robotic systems?",
+            type = "TEXTUAL",
+            context = QuestionContextEnum.EXTRACTION
+        )
+
+        val eq2 = question.createQuestion(
+            reviewId = systematicId,
+            code = "EQ2",
+            title = "What is the most common way of interaction among service-oriented robotic systems?",
+            type = "NUMBERED_SCALE",
+            context = QuestionContextEnum.EXTRACTION,
+            higher = 5,
+            lower = 1
+        )
+
+        val eq3 = question.createQuestion(
+            reviewId = systematicId,
+            code = "EQ3",
+            title = "What implementation technology has been mostly used to develop service-oriented robotic systems?",
+            type = "LABELED_SCALE",
+            context = QuestionContextEnum.EXTRACTION,
+            scales = mapOf(
+                "How clearly are the review’s research objectives stated?" to 1,
+                "How comprehensive is the search strategy (databases, keywords, and search strings)?" to 2,
+                "How transparent is the data extraction and synthesis process?" to 3,
+                "How well are the findings (tables, graphs, mapping) organized and reported?" to 4,
+                "How adequately are the threats to validity discussed?" to 5
+            )
+        )
+
+        val eq4 = question.createQuestion(
+            reviewId = systematicId,
+            code = "EQ4",
+            title = "Which research question provides the most valuable insight for future research?",
+            type = "PICK_LIST",
+            context = QuestionContextEnum.EXTRACTION,
+            options = listOf(
+                "RQ1: How has service-orientation been applied to the development of robotic systems?",
+                "RQ2: What is the most common way of interaction among service-oriented robotic systems?",
+                "RQ3: What implementation technology has been mostly used to develop service-oriented robotic systems?",
+                "RQ4: What are the development environments and tools that support the development of service-oriented robotic systems?",
+                "RQ5: Is SOA applicable to all types of robots?",
+                "RQ6: How has Software Engineering been applied to the development of service-oriented robotic systems?"
+            )
+        )
+
+        val rbq1 = question.createQuestion(
+            reviewId = systematicId,
+            code = "RBQ1",
+            title = "In your own words, describe potential sources of selection bias in this review.",
+            type = "TEXTUAL",
+            context = QuestionContextEnum.ROB
+        )
+
+        val rbq2 = question.createQuestion(
+            reviewId = systematicId,
+            code = "RBQ2",
+            title = "Which option best describes the overall risk of bias in the review?",
+            type = "PICK_LIST",
+            context = QuestionContextEnum.ROB,
+            options = listOf(
+                "Very High",
+                "High",
+                "Moderate",
+                "Low",
+                "Very Low"
+            )
+        )
+
+        val rbq3 = question.createQuestion(
+            reviewId = systematicId,
+            code = "RBQ3",
+            title = "How effectively is publication bias minimized (e.g., through multiple databases and specialist suggestions)?",
+            type = "LABELED_SCALE",
+            context = QuestionContextEnum.ROB,
+            scales = mapOf(
+                "Not effective at all" to 1,
+                "Slightly effective" to 2,
+                "Moderately effective" to 3,
+                "Very effective" to 4,
+                "Extremely effective" to 5
+            )
+        )
+
+        val rbq4 = question.createQuestion(
+            reviewId = systematicId,
+            code = "RBQ4",
+            title = "Rate the overall reliability of the review process from 1 (very unreliable) to 5 (very reliable).",
+            type = "NUMBERED_SCALE",
+            context = QuestionContextEnum.ROB,
+            higher = 5,
+            lower = 1
+        )
+
         protocolRepository.saveOrUpdate(
             ProtocolDto(
-                id = systematicStudyId,
-                systematicStudy = systematicStudyId,
+                id = systematicId,
+                systematicStudy = systematicId,
                 goal = "The main objective of this systematic review is to identify primary studies that report the development of service-oriented robotic systems, i.e., robotic systems that are designed based on SOA. Furthermore, the review aims to obtain a comprehensive overview of the technologies and methodologies used to develop such systems.",
                 justification = "Despite a growing number of studies in service-oriented robotics, there is no complete and detailed overview of the systems, implementation technologies, and software engineering guidelines in this domain.",
                 researchQuestions = setOf(
@@ -76,8 +175,8 @@ class CreateSystematicReviewExampleService(
                 ),
                 dataCollectionProcess = "Data extraction will be performed using pre-defined extraction tables corresponding to each research question.",
                 analysisAndSynthesisProcess = "Data were synthesized using statistical methods and meta-analysis to draw conclusions about the research area.",
-                extractionQuestions = setOf(),
-                robQuestions = setOf(),
+                extractionQuestions = setOf(eq1.questionId, eq2.questionId, eq3.questionId, eq4.questionId),
+                robQuestions = setOf(rbq1.questionId, rbq2.questionId, rbq3.questionId, rbq4.questionId),
                 picoc = PicocDto(
                     population = "Researchers and developers of robotic systems interested in employing SOA.",
                     intervention = "The development and use of service-oriented robotic systems.",
@@ -87,6 +186,6 @@ class CreateSystematicReviewExampleService(
                 )
             )
         )
-        return systematicStudyId
+        return systematicId
     }
 }
