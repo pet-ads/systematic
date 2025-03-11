cd7e94cc231978562acb345173ca9cfd7a7b6387
ericksgmes <erick.dicksan@gmail.com>
2025-03-11 20:22:49 -0300

    feat: add source parameter to extractReferences method

--- review/src/main/kotlin/br/all/domain/services/ConverterFactoryService.kt
+++ review/src/main/kotlin/br/all/domain/services/ConverterFactoryService.kt
@@ -12,14 +12,15 @@ class ConverterFactoryService(
     fun extractReferences(
         systematicStudyId: SystematicStudyId,
         searchSessionId: SearchSessionID,
-        file: String
+        file: String,
+        source: MutableSet<String>
     ): Pair<List<StudyReview>, List<String>> {
         val bibtexPattern = Regex("""@\s*(article|book|conference|inproceedings|techreport|phdthesis|mastersthesis|incollection)\s*""", RegexOption.IGNORE_CASE)
         val risPattern = Regex("""^TY\s+-\s+""", RegexOption.MULTILINE)
 
         return when {
-            bibtexPattern.containsMatchIn(file) -> bibtexConverterService.convertManyToStudyReview(systematicStudyId, searchSessionId, file)
-            risPattern.containsMatchIn(file) -> risConverterService.convertManyToStudyReview(systematicStudyId, searchSessionId, file)
+            bibtexPattern.containsMatchIn(file) -> bibtexConverterService.convertManyToStudyReview(systematicStudyId, searchSessionId, file, source)
+            risPattern.containsMatchIn(file) -> risConverterService.convertManyToStudyReview(systematicStudyId, searchSessionId, file, source)
             else -> {
                 Pair(emptyList(), listOf("File format not recognized or unsupported"))
             }
