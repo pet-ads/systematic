24385f02ba2eadf453f500536cd7b2e0dad94f70
ericksgmes <erick.dicksan@gmail.com>
2025-03-11 12:21:50 -0300

    refactor: change class to correctly append the search sources to the selected study, also to mark only payload studies as duplicates

--- review/src/main/kotlin/br/all/application/study/update/implementation/MarkAsDuplicatedServiceImpl.kt
+++ review/src/main/kotlin/br/all/application/study/update/implementation/MarkAsDuplicatedServiceImpl.kt
@@ -9,16 +9,14 @@ import br.all.application.study.repository.fromDto
 import br.all.application.study.repository.toDto
 import br.all.application.study.update.interfaces.MarkAsDuplicatedPresenter
 import br.all.application.study.update.interfaces.MarkAsDuplicatedService
-import br.all.application.study.update.interfaces.MarkAsDuplicatedService.ResponseModel
 import br.all.application.user.CredentialsService
 import br.all.domain.model.review.SystematicStudy
 import br.all.domain.model.study.StudyReview
-import java.util.UUID
 
 class MarkAsDuplicatedServiceImpl(
-    val systematicStudyRepository: SystematicStudyRepository,
-    val studyReviewRepository: StudyReviewRepository,
-    val credentialsService: CredentialsService,
+    private val systematicStudyRepository: SystematicStudyRepository,
+    private val studyReviewRepository: StudyReviewRepository,
+    private val credentialsService: CredentialsService,
 ) : MarkAsDuplicatedService {
 
     override fun markAsDuplicated(
@@ -35,26 +33,28 @@ class MarkAsDuplicatedServiceImpl(
 
         val referenceStudyDto = studyReviewRepository.findById(request.systematicStudyId, request.referenceStudyId)
         if (referenceStudyDto == null) {
-            val message = "Reference study review with id ${request.referenceStudyId} not found."
-            presenter.prepareFailView(EntityNotFoundException(message))
+            presenter.prepareFailView(EntityNotFoundException("Reference study review with id ${request.referenceStudyId} not found."))
             return
         }
         val referenceStudyReview = StudyReview.fromDto(referenceStudyDto)
 
-        request.duplicatedStudyIds.forEach { duplicatedStudyId ->
-            val duplicatedStudyDto = studyReviewRepository.findById(request.systematicStudyId, duplicatedStudyId)
-            if (duplicatedStudyDto == null) {
-                val message = "Study review to be marked as duplicated with id $duplicatedStudyId not found."
-                presenter.prepareFailView(EntityNotFoundException(message))
+        val duplicates = request.duplicatedStudyIds.map { duplicateId ->
+            val dupDto = studyReviewRepository.findById(request.systematicStudyId, duplicateId)
+            if (dupDto == null) {
+                presenter.prepareFailView(EntityNotFoundException("Study review to be marked as duplicated with id $duplicateId not found."))
                 return
             }
-            val duplicatedStudyReview = StudyReview.fromDto(duplicatedStudyDto)
-            duplicatedStudyReview.markAsDuplicated(referenceStudyReview)
-
-            studyReviewRepository.saveOrUpdate(duplicatedStudyReview.toDto())
+            StudyReview.fromDto(dupDto)
         }
 
-        val response = ResponseModel(
+        referenceStudyReview.markAsDuplicated(duplicates)
+
+        duplicates.forEach { duplicate ->
+            studyReviewRepository.saveOrUpdate(duplicate.toDto())
+        }
+        studyReviewRepository.saveOrUpdate(referenceStudyReview.toDto())
+
+        val response = MarkAsDuplicatedService.ResponseModel(
             userId = request.userId,
             systematicStudyId = request.systematicStudyId,
             duplicatedStudies = request.duplicatedStudyIds
@@ -62,3 +62,4 @@ class MarkAsDuplicatedServiceImpl(
         presenter.prepareSuccessView(response)
     }
 }
+
