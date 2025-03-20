75f378c03bb9c1a265e08e19c51b36463772c4aa
ericksgmes <erick.dicksan@gmail.com>
2025-03-20 14:13:15 -0300

    feat: add studyReviewId to PatchStatusStudyReviewRequest

--- web/src/main/kotlin/br/all/study/requests/PatchStatusStudyReviewRequest.kt
+++ web/src/main/kotlin/br/all/study/requests/PatchStatusStudyReviewRequest.kt
@@ -4,10 +4,11 @@ import br.all.application.study.update.interfaces.UpdateStudyReviewStatusService
 import java.util.*
 
 data class PatchStatusStudyReviewRequest(
+    val studyReviewId: List<Long>,
     val status: String,
     val criteria: Set<String>
 ) {
-    fun toRequestModel(userId: UUID, systematicStudyId: UUID, studyReviewId: Long)
+    fun toRequestModel(userId: UUID, systematicStudyId: UUID)
     = UpdateStudyReviewStatusService.RequestModel(
         userId,
         systematicStudyId,
