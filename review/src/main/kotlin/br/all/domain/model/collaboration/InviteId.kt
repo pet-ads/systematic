48d79a9014accdd6cb0cc7cb59ad8edb30dd45e5
gabrmanh <gmanhani@yahoo.com>
2025-04-07 20:20:29 -0300

    feat: define base of invite entity

---
+++ review/src/main/kotlin/br/all/domain/model/collaboration/InviteId.kt
@@ -0,0 +1,21 @@
+package br.all.domain.model.collaboration
+
+import br.all.domain.shared.ddd.Identifier
+import br.all.domain.shared.ddd.Notification
+import java.util.*
+
+@JvmInline
+value class InviteId(private val value: UUID) : Identifier<UUID> {
+    init {
+        val notification = validate()
+        require(notification.hasNoErrors()) { notification.message() }
+    }
+
+    override fun validate() = Notification()
+
+    override fun value() = value
+
+    override fun toString() = value.toString()
+}
+
+fun UUID.toInviteId() = InviteId(this)
