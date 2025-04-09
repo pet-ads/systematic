2e3a634cb57504e31d570ccf0a717bb8249239d1
gabrmanh <gmanhani@yahoo.com>
2025-04-09 19:57:16 -0300

    feat: add validation to invite entity

--- review/src/main/kotlin/br/all/domain/model/collaboration/Invite.kt
+++ review/src/main/kotlin/br/all/domain/model/collaboration/Invite.kt
@@ -2,6 +2,7 @@ package br.all.domain.model.collaboration
 
 import br.all.domain.model.review.SystematicStudyId
 import br.all.domain.shared.ddd.Entity
+import br.all.domain.shared.ddd.Notification
 import br.all.domain.user.UserAccountId
 import java.time.LocalDateTime
 import java.util.*
@@ -10,11 +11,17 @@ class Invite(
     id: InviteId,
     systematicStudyId: SystematicStudyId,
     userId: UserAccountId,
-    inviteDate: LocalDateTime,
-    expirationDate: LocalDateTime = inviteDate.plusDays(30)
+    val inviteDate: LocalDateTime,
+    val expirationDate: LocalDateTime = inviteDate.plusDays(30)
 ) : Entity<UUID>(id) {
-    
-    init{
-        require(expirationDate.isAfter(inviteDate))
+
+    init {
+        val notification = validate()
+        require(notification.hasNoErrors()) { notification.message() }
+    }
+
+    fun validate() = Notification().also {
+        if (inviteDate.isAfter(expirationDate)) 
+            it.addError("Invite date cannot be after expiration date")
     }
 }
