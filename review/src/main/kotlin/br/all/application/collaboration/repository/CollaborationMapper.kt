e4dc6b44a8593d0dd6ff0b6d7d77d3794ddea8f9
gabrmanh <gmanhani@yahoo.com>
2025-04-11 20:08:04 -0300

    feat: implement mapper for collaboration entities and dtos

---
+++ review/src/main/kotlin/br/all/application/collaboration/repository/CollaborationMapper.kt
@@ -0,0 +1,37 @@
+package br.all.application.collaboration.repository
+
+import br.all.domain.model.collaboration.*
+import br.all.domain.model.review.toSystematicStudyId
+import br.all.domain.model.user.toResearcherId
+
+fun Invite.toDto(): InviteDto = InviteDto(
+    id = this.id.value(),
+    systematicStudyId = this.systematicStudyId.value(),
+    userId = this.userId.value,
+    inviteDate = this.inviteDate,
+    expirationDate = this.expirationDate
+)
+
+fun InviteDto.toDomain(): Invite = Invite(
+    id = this.id.toInviteId(),
+    systematicStudyId = this.systematicStudyId.toSystematicStudyId(),
+    userId = this.userId.toResearcherId(),
+    inviteDate = this.inviteDate,
+    expirationDate = this.expirationDate
+)
+
+fun Collaboration.toDto(): CollaborationDto = CollaborationDto(
+    id = this.id.value(),
+    systematicStudyId = this.systematicStudyId.value(),
+    userId = this.userId.value,
+    status = this.status.name,
+    permissions = this.permissions.map { it.name }.toSet()
+)
+
+fun CollaborationDto.toDomain(): Collaboration = Collaboration(
+    id = this.id.toCollaborationId(),
+    systematicStudyId = this.systematicStudyId.toSystematicStudyId(),
+    userId = this.userId.toResearcherId(),
+    status = CollaborationStatus.valueOf(this.status),
+    permissions = this.permissions.map { CollaborationPermission.valueOf(it) }.toSet()
+)
