c3633b02ba785e1d342f2c5bdab0796395f75930
ericksgmes <erick.dicksan@gmail.com>
2025-04-12 09:06:45 -0300

    refactor: remove dependency injection IdGeneratorService

--- review/src/main/kotlin/br/all/application/report/find/service/FindAnswersServiceImpl.kt
+++ review/src/main/kotlin/br/all/application/report/find/service/FindAnswersServiceImpl.kt
@@ -4,14 +4,12 @@ import br.all.application.question.repository.QuestionRepository
 import br.all.application.report.find.presenter.FindAnswersPresenter
 import br.all.application.study.repository.StudyReviewRepository
 import br.all.application.user.CredentialsService
-import br.all.domain.services.IdGeneratorService
 import java.util.*
 
 class FindAnswersServiceImpl(
     private val questionRepository: QuestionRepository,
     private val studyReviewRepository: StudyReviewRepository,
     private val credentialsService: CredentialsService,
-    private val idGenerator: IdGeneratorService
 ): FindAnswersService {
 
     override fun findAnswers(presenter: FindAnswersPresenter, request: FindAnswersService.RequestModel) {
