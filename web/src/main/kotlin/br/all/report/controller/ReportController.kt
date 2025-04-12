4594f18c9e25a51bb4548309c0e759f4d5b70828
ericksgmes <erick.dicksan@gmail.com>
2025-04-12 09:07:29 -0300

    feat: create controller method findCriteria

--- web/src/main/kotlin/br/all/report/controller/ReportController.kt
+++ web/src/main/kotlin/br/all/report/controller/ReportController.kt
@@ -1,7 +1,9 @@
 package br.all.report.controller
 
 import br.all.application.report.find.service.FindAnswersService
+import br.all.application.report.find.service.FindCriteriaService
 import br.all.report.presenter.RestfulFindAnswersPresenter
+import br.all.report.presenter.RestfulFindCriteriaPresenter
 import br.all.security.service.AuthenticationInfoService
 import br.all.utils.LinksFactory
 import io.swagger.v3.oas.annotations.Operation
@@ -18,14 +20,15 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
-@RequestMapping("/api/v1/systematic-study/{systematicStudy}/report")
+@RequestMapping("/api/v1/systematic-study/{systematicStudyId}/report/")
 class ReportController(
     private val findAnswersService: FindAnswersService,
+    private val findCriteriaService: FindCriteriaService,
     private val authenticationInfoService: AuthenticationInfoService,
     private val linksFactory: LinksFactory
 ) {
 
-    @GetMapping("question/{studyReviewId}/{questionId}")
+    @GetMapping("{studyReviewId}/question/{questionId}")
     @Operation(summary = "Retrieve all question answers")
     @ApiResponses(
         value = [
@@ -37,21 +40,57 @@ class ReportController(
                     schema = Schema(implementation = RestfulFindAnswersPresenter::class)
                 )]
             ),
-            ApiResponse(responseCode = "401", description = "Fail getting all question answers - unauthenticated user",
+            ApiResponse(responseCode = "401", description = "Failed getting all question answers - unauthenticated user",
                 content = [Content(schema = Schema(hidden = true))]),
-            ApiResponse(responseCode = "403", description = "Fail getting all question answers - unauthorized user",
+            ApiResponse(responseCode = "403", description = "Failed getting all question answers - unauthorized user",
                 content = [Content(schema = Schema(hidden = true))]),
         ]
     )
     fun findAnswers(
-        @PathVariable systematicStudy: UUID,
+        @PathVariable systematicStudyId: UUID,
         @PathVariable studyReviewId: Long,
         @PathVariable questionId: UUID,
     ): ResponseEntity<*>{
         val presenter = RestfulFindAnswersPresenter(linksFactory)
         val userId = authenticationInfoService.getAuthenticatedUserId()
-        val request = FindAnswersService.RequestModel(userId, systematicStudy, studyReviewId, questionId)
+        val request = FindAnswersService.RequestModel(userId, systematicStudyId, studyReviewId, questionId)
         findAnswersService.findAnswers(presenter, request)
+        return presenter.responseEntity ?: ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @GetMapping("{studyReviewId}/criteria/{type}")
+    @Operation(summary = "Get all studies included or excluded by criteria")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Success getting all studies included or excluded by criteria",
+                content = [Content(
+                    mediaType = "application/json",
+                    schema = Schema(implementation = RestfulFindCriteriaPresenter::class)
+                )]
+            ),
+            ApiResponse(
+                responseCode = "401",
+                description = "Failed to get all studies included or excluded by criteria - unauthenticated user",
+                content = [Content(schema = Schema(hidden = true)
+                )]),
+            ApiResponse(
+                responseCode = "403",
+                description = "Failed getting all studies included or excluded by criteria - unauthorized user",
+                content = [Content(schema = Schema(hidden = true)
+                )]),
+        ]
+    )
+    fun findCriteria(
+        @PathVariable systematicStudyId: UUID,
+        @PathVariable studyReviewId: Long,
+        @PathVariable type: String,
+    ): ResponseEntity<*> {
+        val presenter = RestfulFindCriteriaPresenter(linksFactory)
+        val userId = authenticationInfoService.getAuthenticatedUserId()
+        val request = FindCriteriaService.RequestModel(userId, systematicStudyId, studyReviewId, type)
+        findCriteriaService.findCriteria(presenter, request)
         return presenter.responseEntity ?: ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
