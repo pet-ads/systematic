2488085f7ec479690ad5699bbb2e92b01366d435
matheusspacifico <matheuspacifico44@gmail.com>
2025-04-18 19:36:59 -0300

    feat(ReviewSimilarityService): add initial logic to detectDuplicates function

--- review/src/main/kotlin/br/all/domain/services/ReviewSimilarityService.kt
+++ review/src/main/kotlin/br/all/domain/services/ReviewSimilarityService.kt
@@ -1,18 +1,38 @@
 package br.all.domain.services
 
 import br.all.domain.model.study.StudyReview
+import org.apache.commons.text.similarity.LevenshteinDistance
+import java.text.Normalizer
 
 class ReviewSimilarityService {
-    private val titleThreshold: Double = 0.8
-    private val authorsThreshold: Double = 0.8
-    private val abstractThreshold: Double = 0.8
+    private val titleThreshold: Double = 0.9
+    private val authorsThreshold: Double = 0.9
+    private val abstractThreshold: Double = 0.9
 
     public fun detectDuplicates(studies: List<StudyReview>): Map<StudyReview, Set<StudyReview>> {
-        TODO()
-    }
+        val duplicatedReviewsMap = mutableMapOf<StudyReview, MutableSet<StudyReview>>()
+        val remainingStudies = studies.toMutableList()
 
-    private fun compareTwoStudies(study1: StudyReview, study2: StudyReview): Boolean {
-        TODO()
+        while (remainingStudies.isNotEmpty()) {
+            remainingStudies.removeFirst().let { a ->
+                val duplicatesOfA = mutableSetOf<StudyReview>()
+                val iterator = remainingStudies.iterator()
+
+                while (iterator.hasNext()) {
+                    val b = iterator.next()
+                    if (calculateTitleSimilarity(a, b) >= titleThreshold && calculateAuthorsSimilarity(a, b) >= authorsThreshold) {
+                        if (calculateAbstractSimilarity(a, b) >= abstractThreshold) {
+                            duplicatesOfA.add(b)
+                            iterator.remove()
+                        }
+                    }
+                }
+
+                if (duplicatesOfA.isNotEmpty()) duplicatedReviewsMap[a] = duplicatesOfA
+            }
+        }
+
+        return duplicatedReviewsMap
     }
 
     private fun calculateTitleSimilarity(study1: StudyReview, study2: StudyReview): Double {
@@ -24,6 +44,14 @@ class ReviewSimilarityService {
     }
 
     private fun calculateAbstractSimilarity(study1: StudyReview, study2: StudyReview): Double {
+        TODO()
+    }
+
+    private fun normalizeText(text: String): String {
+        TODO()
+    }
+
+    private fun similarity(text1: String, text2: String): Double {
         TODO()
     }
 }
