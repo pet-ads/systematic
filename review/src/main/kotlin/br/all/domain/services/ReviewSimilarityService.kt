c539f00f78fe45695e3231099c7a69350f76e75a
matheusspacifico <matheuspacifico44@gmail.com>
2025-04-18 19:37:34 -0300

    feat(ReviewSimilarityService): implement levenshtein distance logic function

--- review/src/main/kotlin/br/all/domain/services/ReviewSimilarityService.kt
+++ review/src/main/kotlin/br/all/domain/services/ReviewSimilarityService.kt
@@ -52,6 +52,16 @@ class ReviewSimilarityService {
     }
 
     private fun similarity(text1: String, text2: String): Double {
-        TODO()
+        val normalizedText1 = normalizeText(text1)
+        val normalizedText2 = normalizeText(text2)
+
+        if (normalizedText1.isEmpty() && normalizedText2.isEmpty()) return 1.0
+        if (normalizedText1.isEmpty() || normalizedText2.isEmpty()) return 0.0
+
+        val levenshtein = LevenshteinDistance.getDefaultInstance()
+        val distance = levenshtein.apply(normalizedText1, normalizedText2).toDouble()
+        val lengthOfLargerString = maxOf(normalizedText1.length, normalizedText2.length)
+
+        return 1.0 - (distance / lengthOfLargerString)
     }
 }
