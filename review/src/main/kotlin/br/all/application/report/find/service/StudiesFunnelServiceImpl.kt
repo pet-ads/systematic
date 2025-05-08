b087af9c17d1fc1ef1caf6813026d085a73315c9
ericksgmes <erick.dicksan@gmail.com>
2025-05-08 11:08:00 -0300

    style: remove comments after revision

--- review/src/main/kotlin/br/all/application/report/find/service/StudiesFunnelServiceImpl.kt
+++ review/src/main/kotlin/br/all/application/report/find/service/StudiesFunnelServiceImpl.kt
@@ -34,44 +34,36 @@ class StudiesFunnelServiceImpl(
     }
 
     private fun createResponse(allStudies: List<StudyReviewDto>, request: StudiesFunnelService.RequestModel): StudiesFunnelService.ResponseModel {
-        // Total de estudos encontrados, fazer por source
         val totalIdentifiedBySource = allStudies
             .flatMap { it.searchSources }
             .groupingBy { it }
             .eachCount()
 
-        // Remover os duplicados do total, e depois, fazer por source
         val nonDuplicatedStudies = allStudies.filter { it.selectionStatus != SelectionStatus.DUPLICATED.name }
         val totalAfterDuplicatesRemovedBySource = nonDuplicatedStudies
             .flatMap { it.searchSources }
             .groupingBy { it }
             .eachCount()
 
-        // Total analisado na selection
         val totalScreened = allStudies.size
 
-        // Total excluídos na selection
         val totalExcludedInScreening = allStudies.count { it.selectionStatus == SelectionStatus.EXCLUDED.name }
 
-        // Dos excluídos na selection, fazer por criteria
         val excludedByCriterion = allStudies
             .filter { it.selectionStatus == SelectionStatus.EXCLUDED.name }
             .flatMap { it.criteria }
             .groupingBy { it }
             .eachCount()
 
-        // Total analisados na extraction (passou da selection)
         val totalFullTextAssessed = allStudies.count { 
             it.selectionStatus == SelectionStatus.INCLUDED.name 
         }
 
-        // Total excluídos na extraction
         val totalExcludedInFullText = allStudies.count { 
             it.selectionStatus == SelectionStatus.INCLUDED.name && 
             it.extractionStatus == ExtractionStatus.EXCLUDED.name 
         }
 
-        // Dos excluídos na extraction, fazer por criteria
         val totalExcludedByCriterion = allStudies
             .filter { 
                 it.selectionStatus == SelectionStatus.INCLUDED.name && 
@@ -81,7 +73,6 @@ class StudiesFunnelServiceImpl(
             .groupingBy { it }
             .eachCount()
 
-        // Total incluído (passou na selection e extraction)
         val totalIncluded = allStudies.count { 
             it.selectionStatus == SelectionStatus.INCLUDED.name && 
             it.extractionStatus == ExtractionStatus.INCLUDED.name 
