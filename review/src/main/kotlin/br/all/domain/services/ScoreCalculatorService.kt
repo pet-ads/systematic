be73bd2ddbdb8bba26758a4eed5dac3738240c65
matheusspacifico <matheuspacifico44@gmail.com>
2025-05-09 11:54:37 -0300

    refactor(score): modify service to make it stateless

--- review/src/main/kotlin/br/all/domain/services/ScoreCalculatorService.kt
+++ review/src/main/kotlin/br/all/domain/services/ScoreCalculatorService.kt
@@ -2,37 +2,35 @@ package br.all.domain.services
 
 import br.all.domain.model.study.StudyReview
 import br.all.domain.shared.utils.normalizeText
+import org.springframework.stereotype.Service
 
-class ScoreCalculatorService(
-    private val protocolKeywords: Set<String>?
-) {
-    private fun calculateScore(title: String, abstract: String?, authorsKeywords: Set<String>): Int {
+@Service
+class ScoreCalculatorService {
+    private fun calculateScore(title: String, abstract: String?, authorsKeywords: Set<String>, protocolKeywords: Set<String>): Int {
         var score = 0
 
         val normalizedTitle = normalizeText(title)
         val normalizedAbstract = abstract?.let { normalizeText(it) } ?: ""
         val normalizedAuthorsKeywords = authorsKeywords.map { normalizeText(it) }.toSet()
 
-        if (protocolKeywords != null) {
-            for (keyword in protocolKeywords) {
-                val normalizedKeyword = normalizeText(keyword)
-                val regex = Regex("\\b$normalizedKeyword\\b", RegexOption.IGNORE_CASE)
+        for (keyword in protocolKeywords) {
+            val normalizedKeyword = normalizeText(keyword)
+            val regex = Regex("\\b$normalizedKeyword\\b", RegexOption.IGNORE_CASE)
 
-                score += regex.findAll(normalizedTitle).count() * 5
-                score += regex.findAll(normalizedAbstract).count() * 3
-                if (normalizedAuthorsKeywords.any { regex.containsMatchIn(it) }) score += 2
-            }
+            score += regex.findAll(normalizedTitle).count() * 5
+            score += regex.findAll(normalizedAbstract).count() * 3
+            if (normalizedAuthorsKeywords.any { regex.containsMatchIn(it) }) score += 2
         }
 
         return score
     }
 
-    private fun applyScoreToStudyReview(studyReview: StudyReview): StudyReview {
-        val score = calculateScore(studyReview.title, studyReview.abstract, studyReview.keywords)
+    private fun applyScoreToStudyReview(studyReview: StudyReview, protocolKeywords: Set<String>): StudyReview {
+        val score = calculateScore(studyReview.title, studyReview.abstract, studyReview.keywords, protocolKeywords)
         return studyReview.apply { this.score = score }
     }
 
-    fun applyScoreToManyStudyReviews(studyReviews: List<StudyReview>): List<StudyReview> {
-        return studyReviews.map { applyScoreToStudyReview(it) }
+    fun applyScoreToManyStudyReviews(studyReviews: List<StudyReview>, protocolKeywords: Set<String>): List<StudyReview> {
+        return studyReviews.map { applyScoreToStudyReview(it, protocolKeywords) }
     }
 }
