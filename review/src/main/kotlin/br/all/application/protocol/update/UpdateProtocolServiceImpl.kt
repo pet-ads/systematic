17521820d68c0896a4db3ad100a5891b77108734
matheusspacifico <matheuspacifico44@gmail.com>
2025-05-09 11:56:28 -0300

    refactor(score): modify protocol update service to adapt new stateless service

--- review/src/main/kotlin/br/all/application/protocol/update/UpdateProtocolServiceImpl.kt
+++ review/src/main/kotlin/br/all/application/protocol/update/UpdateProtocolServiceImpl.kt
@@ -24,6 +24,7 @@ class UpdateProtocolServiceImpl(
     private val systematicStudyRepository: SystematicStudyRepository,
     private val credentialsService: CredentialsService,
     private val studyReviewRepository: StudyReviewRepository,
+    private val scoreCalculatorService: ScoreCalculatorService
 ) : UpdateProtocolService {
     override fun update(presenter: UpdateProtocolPresenter, request: RequestModel) {
         val userId = request.userId
@@ -50,13 +51,11 @@ class UpdateProtocolServiceImpl(
         if (updatedDto != dto) protocolRepository.saveOrUpdate(updatedDto)
 
         if (keywordsUpdated) {
-            val scoreCalculatorService = ScoreCalculatorService(updatedDto.keywords)
-
             val studyReviewDtos = studyReviewRepository.findAllFromReview(systematicStudyId)
 
             val studyReviews = studyReviewDtos.map { StudyReview.fromDto(it) }
 
-            val updatedStudyReviews = scoreCalculatorService.applyScoreToManyStudyReviews(studyReviews)
+            val updatedStudyReviews = scoreCalculatorService.applyScoreToManyStudyReviews(studyReviews, protocol.keywords)
 
             val updatedStudyReviewDtos = updatedStudyReviews.map { it.toDto() }
             studyReviewRepository.saveOrUpdateBatch(updatedStudyReviewDtos)
