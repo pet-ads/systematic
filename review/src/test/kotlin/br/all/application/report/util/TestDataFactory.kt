48602cdfeef832b8d4c7decc75a2a35abcdf841a
matheusspacifico <matheuspacifico44@gmail.com>
2025-05-16 20:16:26 -0300

    feat(report/test): add test data factory for report tests

---
+++ review/src/test/kotlin/br/all/application/report/util/TestDataFactory.kt
@@ -0,0 +1,9 @@
+package br.all.application.report.util
+
+import java.util.UUID
+
+class TestDataFactory {
+
+    val researcher: UUID = UUID.randomUUID()
+    val systematicStudy: UUID = UUID.randomUUID()
+}
