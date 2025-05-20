92e04c30b57228902219bb7e5f7787fdd86eb5a7
ericksgmes <erick.dicksan@gmail.com>
2025-05-20 13:06:43 -0300

    refactor: change location of function

--- review/src/main/kotlin/br/all/application/report/find/service/ExportProtocolServiceImpl.kt
+++ review/src/main/kotlin/br/all/application/report/find/service/ExportProtocolServiceImpl.kt
@@ -5,6 +5,7 @@ import br.all.application.protocol.repository.ProtocolRepository
 import br.all.application.report.find.presenter.ExportProtocolPresenter
 import br.all.application.review.repository.SystematicStudyRepository
 import br.all.application.review.repository.fromDto
+import br.all.application.shared.exceptions.EntityNotFoundException
 import br.all.application.shared.presenter.prepareIfFailsPreconditions
 import br.all.application.user.CredentialsService
 import br.all.domain.model.review.SystematicStudy
@@ -34,6 +35,12 @@ class ExportProtocolServiceImpl(
             protocolDto.toFto(),
         )
 
+        if (formattedProtocol == null) {
+            val message = "Invalid export format ${request.format}"
+            presenter.prepareFailView(EntityNotFoundException(message))
+            return
+        }
+
         val response = ExportProtocolService.ResponseModel(
             userId = request.userId,
             systematicStudyId = request.systematicStudyId,
@@ -43,28 +50,28 @@ class ExportProtocolServiceImpl(
 
         presenter.prepareSuccessView(response)
     }
-}
 
-fun ProtocolDto.toFto(): ProtocolFto {
-    return ProtocolFto(
-        id = this.id.toString(),
-        systematicStudy = this.systematicStudy.toString(),
-        goal = this.goal.orEmpty(),
-        justification = this.justification.orEmpty(),
-        researchQuestions = this.researchQuestions.toList(),
-        keywords = this.keywords.toList(),
-        searchString = this.searchString.orEmpty(),
-        informationSources = this.informationSources.toList(),
-        sourcesSelectionCriteria = this.sourcesSelectionCriteria.orEmpty(),
-        searchMethod = this.searchMethod.orEmpty(),
-        studiesLanguages = this.studiesLanguages.toList(),
-        studyTypeDefinition = this.studyTypeDefinition.orEmpty(),
-        selectionProcess = this.selectionProcess.orEmpty(),
-        eligibilityCriteria = this.eligibilityCriteria.map { it.description },
-        dataCollectionProcess = this.dataCollectionProcess.orEmpty(),
-        analysisAndSynthesisProcess = this.analysisAndSynthesisProcess.orEmpty(),
-        extractionQuestions = this.extractionQuestions.map { it.toString() },
-        robQuestions = this.robQuestions.map { it.toString() },
-        picoc = this.picoc?.toString() ?: ""
-    )
+    private fun ProtocolDto.toFto(): ProtocolFto {
+        return ProtocolFto(
+            id = this.id.toString(),
+            systematicStudy = this.systematicStudy.toString(),
+            goal = this.goal.orEmpty(),
+            justification = this.justification.orEmpty(),
+            researchQuestions = this.researchQuestions.toList(),
+            keywords = this.keywords.toList(),
+            searchString = this.searchString.orEmpty(),
+            informationSources = this.informationSources.toList(),
+            sourcesSelectionCriteria = this.sourcesSelectionCriteria.orEmpty(),
+            searchMethod = this.searchMethod.orEmpty(),
+            studiesLanguages = this.studiesLanguages.toList(),
+            studyTypeDefinition = this.studyTypeDefinition.orEmpty(),
+            selectionProcess = this.selectionProcess.orEmpty(),
+            eligibilityCriteria = this.eligibilityCriteria.map { it.description },
+            dataCollectionProcess = this.dataCollectionProcess.orEmpty(),
+            analysisAndSynthesisProcess = this.analysisAndSynthesisProcess.orEmpty(),
+            extractionQuestions = this.extractionQuestions.map { it.toString() },
+            robQuestions = this.robQuestions.map { it.toString() },
+            picoc = this.picoc?.toString() ?: ""
+        )
+    }
 }
