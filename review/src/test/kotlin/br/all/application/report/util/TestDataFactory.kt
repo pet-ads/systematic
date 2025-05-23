7221c3849c63c5bc954ef58d4da7e85f962e83dc
matheusspacifico <matheuspacifico44@gmail.com>
2025-05-23 13:18:02 -0300

    test(report/util): add question and answer dtos in TestDataFactory

--- review/src/test/kotlin/br/all/application/report/util/TestDataFactory.kt
+++ review/src/test/kotlin/br/all/application/report/util/TestDataFactory.kt
@@ -1,7 +1,11 @@
 package br.all.application.report.util
 
 import br.all.application.protocol.repository.CriterionDto
+import br.all.application.question.repository.QuestionDto
+import br.all.application.study.repository.AnswerDto
+import br.all.domain.model.question.QuestionContextEnum
 import io.github.serpro69.kfaker.Faker
+import java.util.*
 
 class TestDataFactory {
     val faker = Faker()
@@ -13,6 +17,38 @@ class TestDataFactory {
         return CriterionDto(
             description = description,
             type = type,
+        )
+    }
+
+    fun questionDto(
+        questionId: UUID = UUID.randomUUID(),
+        systematicStudyId: UUID = UUID.randomUUID(),
+        code: String = faker.lorem.words(),
+        description: String = faker.lorem.words(),
+        questionType: String = "TEXTUAL",
+        questionContext: QuestionContextEnum = QuestionContextEnum.EXTRACTION,
+    ): QuestionDto {
+        return QuestionDto(
+            questionId = questionId,
+            systematicStudyId = systematicStudyId,
+            code = code,
+            description = description,
+            questionType = questionType,
+            scales = null,
+            higher = null,
+            lower = null,
+            options = null,
+            context = questionContext
+        )
+    }
+
+    fun answerDto(
+        studyReviewId: Long = faker.random.nextLong(),
+        answer: String = faker.lorem.words()
+    ): AnswerDto {
+        return AnswerDto(
+            studyReviewId = studyReviewId,
+            answer = answer
         )
     }
 }
