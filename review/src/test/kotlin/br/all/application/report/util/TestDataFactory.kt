86631cc13f5d46b0bf70c24f071943b5f687cbed
ericksgmes <erick.dicksan@gmail.com>
2025-06-01 23:57:17 -0300

    test: create expected response function

--- review/src/test/kotlin/br/all/application/report/util/TestDataFactory.kt
+++ review/src/test/kotlin/br/all/application/report/util/TestDataFactory.kt
@@ -2,7 +2,10 @@ package br.all.application.report.util
 
 import br.all.application.protocol.repository.CriterionDto
 import br.all.application.question.repository.QuestionDto
+import br.all.application.report.find.service.IncludedStudiesAnswersService
+import br.all.application.report.find.service.IncludedStudiesAnswersService.QuestionWithAnswer
 import br.all.application.study.repository.AnswerDto
+import br.all.application.study.repository.StudyReviewDto
 import br.all.domain.model.question.QuestionContextEnum
 import io.github.serpro69.kfaker.Faker
 import java.util.UUID
@@ -50,6 +53,46 @@ class TestDataFactory {
         return AnswerDto(
             studyReviewId = studyReviewId,
             answer = answer
+        )
+    }
+
+    fun expectedIncludedStudiesResponse(
+        studyReviewDto: StudyReviewDto,
+        userId: UUID = UUID.randomUUID(),
+        extractionQuestions: List<QuestionDto> = emptyList(),
+        robQuestions: List<QuestionDto> = emptyList(),
+    ): IncludedStudiesAnswersService.ResponseModel {
+        val extractionAnswers: MutableList<QuestionWithAnswer> = mutableListOf()
+        val robAnswers: MutableList<QuestionWithAnswer> = mutableListOf()
+
+        extractionQuestions.forEach {
+            extractionAnswers.add(QuestionWithAnswer(
+                questionId = it.questionId,
+                code = it.code,
+                description = it.description,
+                answer = studyReviewDto.formAnswers[it.questionId],
+                type = it.questionType
+            ))
+        }
+
+        robQuestions.forEach {
+            robAnswers.add(QuestionWithAnswer(
+                questionId = it.questionId,
+                code = it.code,
+                description = it.description,
+                answer = studyReviewDto.robAnswers[it.questionId],
+                type = it.questionType
+            ))
+        }
+
+        return IncludedStudiesAnswersService.ResponseModel(
+            userId = userId,
+            systematicStudyId = studyReviewDto.systematicStudyId,
+            studyReviewId = studyReviewDto.studyReviewId,
+            year = studyReviewDto.year,
+            includedBy = studyReviewDto.criteria,
+            extractionQuestions = extractionAnswers.toList(),
+            robQuestions = robAnswers.toList()
         )
     }
 }
