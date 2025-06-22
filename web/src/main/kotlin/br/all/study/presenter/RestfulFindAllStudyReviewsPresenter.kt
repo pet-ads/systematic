bc13df23a89b1b6ce9eedbc45dd04ce831f60b8a
ericksgmes <erick.dicksan@gmail.com>
2025-06-21 22:10:12 -0300

    feat: add links to each search source

--- web/src/main/kotlin/br/all/study/presenter/RestfulFindAllStudyReviewsPresenter.kt
+++ web/src/main/kotlin/br/all/study/presenter/RestfulFindAllStudyReviewsPresenter.kt
@@ -4,11 +4,8 @@ import br.all.application.study.find.presenter.FindAllStudyReviewsPresenter
 import br.all.application.study.find.service.FindAllStudyReviewsService.ResponseModel
 import br.all.application.study.repository.StudyReviewDto
 import br.all.shared.error.createErrorResponseFrom
-import br.all.study.controller.StudyReviewController
-import br.all.study.requests.PostStudyReviewRequest
 import br.all.utils.LinksFactory
 import org.springframework.hateoas.RepresentationModel
-import org.springframework.hateoas.server.mvc.linkTo
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
@@ -25,16 +22,21 @@ class RestfulFindAllStudyReviewsPresenter(
         val restfulResponse = ViewModel(response.systematicStudyId, response.studyReviews.size, response.studyReviews)
 
         val selfRef = linksFactory.findAllStudies(response.systematicStudyId)
-        val allBySource = linksFactory.findAllStudiesBySource(response.systematicStudyId, "")
+        val sources = getSources(response.studyReviews)
+        for (source in sources) {
+            restfulResponse.add(linksFactory.findAllStudiesBySource(response.systematicStudyId, source))
+        }
         val createStudyReview = linksFactory.createStudy(response.systematicStudyId)
 
-        restfulResponse.add(selfRef, allBySource, createStudyReview)
+        restfulResponse.add(selfRef, createStudyReview)
         responseEntity = ResponseEntity.status(HttpStatus.OK).body(restfulResponse)
     }
 
     override fun prepareFailView(throwable: Throwable) = run {responseEntity = createErrorResponseFrom(throwable) }
 
     override fun isDone() = responseEntity != null
+
+    fun getSources(studies: List<StudyReviewDto>): List<String> = studies.flatMap { it.searchSources }.distinct()
 
     private data class ViewModel (
         val systematicStudyId : UUID,
