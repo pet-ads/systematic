878e84f802373d955c0c1c083f3c901cdf2aa160
matheusspacifico <matheuspacifico44@gmail.com>
2025-06-26 13:56:30 -0300

    test(email): modify tests with false premises

--- review/src/test/kotlin/br/all/domain/shared/ddd/EmailTest.kt
+++ review/src/test/kotlin/br/all/domain/shared/ddd/EmailTest.kt
@@ -61,19 +61,6 @@ class EmailTest {
         exception.message?.contains("Wrong Email format")?.let { assertTrue(it) }
     }
 
-
-    @Test
-    fun `should not accept email with two equal TLDs`() {
-        val exception = assertThrows<IllegalArgumentException> { Email("email@ifsp.com.com") }
-        exception.message?.contains("Wrong Email format")?.let { assertTrue(it) }
-    }
-
-    @Test
-    fun `should not accept email with two equal subdomains`() {
-        val exception = assertThrows<IllegalArgumentException> { Email("email@ifsp.ifsp.com") }
-        exception.message?.contains("Wrong Email format")?.let { assertTrue(it) }
-    }
-
     @Test
     fun `email with incorrect format should include error message`() {
         val exception = assertThrows<IllegalArgumentException> { Email("invalid-email-format") }
@@ -104,7 +91,7 @@ class EmailTest {
 
     @Test
     fun `invalid email with special characters should throw an exception`() {
-        val exception = assertThrows<IllegalArgumentException> { Email("user!name@example.com") }
+        val exception = assertThrows<IllegalArgumentException> { Email("user name@example.com") }
         exception.message?.contains("Wrong Email format")?.let { assertTrue(it) }
     }
 
@@ -151,7 +138,7 @@ class EmailTest {
 
     @Test
     fun `valid email with domain under maximum length should not throw an exception`() {
-        assertDoesNotThrow { Email("user@" + "a".repeat(251) + ".com") }
+        assertDoesNotThrow { Email("user@" + "a".repeat(240) + ".com") }
     }
 
     @Test
@@ -159,6 +146,4 @@ class EmailTest {
         val exception = assertThrows<IllegalArgumentException> { Email("user@domain@com") }
         exception.message?.contains("Wrong Email format")?.let { assertTrue(it) }
     }
-
-
 }
