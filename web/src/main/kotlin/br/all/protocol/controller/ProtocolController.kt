93e537a81aef8c3a5dbd36d0d72ddb3357211c40
matheusspacifico <matheuspacifico44@gmail.com>
2025-06-30 15:03:38 -0300

    feat(protocolStage): add protocol stage endpoint to protocol controller

--- web/src/main/kotlin/br/all/protocol/controller/ProtocolController.kt
+++ web/src/main/kotlin/br/all/protocol/controller/ProtocolController.kt
@@ -1,8 +1,10 @@
 package br.all.protocol.controller
 
 import br.all.application.protocol.find.FindProtocolService
+import br.all.application.protocol.find.GetProtocolStageService
 import br.all.application.protocol.update.UpdateProtocolService
 import br.all.protocol.presenter.RestfulFindProtocolPresenter
+import br.all.protocol.presenter.RestfulGetProtocolStagePresenter
 import br.all.protocol.presenter.RestfulUpdateProtocolPresenter
 import br.all.protocol.requests.PutRequest
 import br.all.security.service.AuthenticationInfoService
@@ -17,12 +19,14 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
 import br.all.application.protocol.find.FindProtocolService.RequestModel as FindOneRequestModel
+import br.all.application.protocol.find.GetProtocolStageService.RequestModel as FindStageRequestModel
 
 @RestController
 @RequestMapping("/systematic-study/{systematicStudyId}/protocol")
 class ProtocolController(
     private val findProtocolService: FindProtocolService,
     private val updateProtocolService: UpdateProtocolService,
+    private val getProtocolStageService: GetProtocolStageService,
     private val authenticationInfoService: AuthenticationInfoService,
     private val linksFactory: LinksFactory
 ) {
@@ -86,4 +90,38 @@ class ProtocolController(
         return presenter.responseEntity ?: ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
+    @GetMapping("/stage")
+    @Operation(summary = "Get the current stage of the systematic review protocol")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Success getting the protocol stage",
+            content = [Content(
+                mediaType = "application/json",
+                schema = Schema(implementation = GetProtocolStageService.ResponseModel::class)
+            )]),
+        ApiResponse(
+            responseCode = "401",
+            description = "Fail getting the protocol stage - unauthenticated collaborator",
+            content = [Content(schema = Schema(hidden = true))]
+        ),
+        ApiResponse(
+            responseCode = "403",
+            description = "Fail getting the protocol stage - unauthorized collaborator",
+            content = [Content(schema = Schema(hidden = true))]
+        ),
+        ApiResponse(
+            responseCode = "404",
+            description = "Fail getting the protocol stage - nonexistent protocol or systematic study",
+            content = [Content(schema = Schema(hidden = true))]
+        ),
+    ])
+    fun getStage(
+        @PathVariable systematicStudyId: UUID
+    ) : ResponseEntity<*> {
+        val presenter = RestfulGetProtocolStagePresenter(linksFactory)
+        val userId = authenticationInfoService.getAuthenticatedUserId()
+        val request = FindStageRequestModel(userId, systematicStudyId)
+
+        getProtocolStageService.getStage(presenter, request)
+        return presenter.responseEntity ?: ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR)
+    }
 }
