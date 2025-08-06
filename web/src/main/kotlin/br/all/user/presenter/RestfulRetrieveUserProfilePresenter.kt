013aa3fed59314eb267566e4b1b7f70e74cfb44a
matheusspacifico <matheuspacifico44@gmail.com>
2025-08-06 15:07:40 -0300

    feat: add restful user profile presenter

---
+++ web/src/main/kotlin/br/all/user/presenter/RestfulRetrieveUserProfilePresenter.kt
@@ -0,0 +1,37 @@
+package br.all.user.presenter
+
+import br.all.application.user.find.RetrieveUserProfileService.ResponseModel
+import br.all.application.user.find.RetrieveUserProfilePresenter
+import br.all.shared.error.createErrorResponseFrom
+import org.springframework.hateoas.RepresentationModel
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.ok
+import java.util.*
+
+class RestfulRetrieveUserProfilePresenter : RetrieveUserProfilePresenter {
+
+    var responseEntity: ResponseEntity<*>? = null
+
+    override fun prepareSuccessView(response: ResponseModel) {
+        val restfulResponse = ViewModel(
+            response.userId,
+            response.username,
+            response.email,
+            response.affiliation,
+            response.country
+        )
+        responseEntity = ok(restfulResponse)
+    }
+
+    override fun prepareFailView(throwable: Throwable) = run { responseEntity = createErrorResponseFrom(throwable) }
+
+    override fun isDone() = responseEntity != null
+
+    private data class ViewModel(
+        val userId: UUID,
+        val username: String,
+        val email: String,
+        val affiliation: String,
+        val country: String
+    ) : RepresentationModel<ViewModel>()
+}
