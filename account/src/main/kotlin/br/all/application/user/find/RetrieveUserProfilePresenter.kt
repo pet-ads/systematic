413f3756096dec5a6ac4835bb6d13010828d7a53
matheusspacifico <matheuspacifico44@gmail.com>
2025-08-06 13:26:45 -0300

    feat: add retrieve user profile presenter and service interfaces

---
+++ account/src/main/kotlin/br/all/application/user/find/RetrieveUserProfilePresenter.kt
@@ -0,0 +1,6 @@
+package br.all.application.user.find
+
+import br.all.application.user.find.RetrieveUserProfileService.ResponseModel
+import br.all.domain.shared.presenter.GenericPresenter
+
+interface RetrieveUserProfilePresenter : GenericPresenter<ResponseModel>
---
+++ account/src/main/kotlin/br/all/application/user/find/RetrieveUserProfileService.kt
@@ -0,0 +1,18 @@
+package br.all.application.user.find
+import java.util.UUID
+
+interface RetrieveUserProfileService {
+    fun retrieveData(presenter: RetrieveUserProfilePresenter, request: RequestModel)
+
+    data class RequestModel(
+        val userId: UUID
+    )
+
+    data class ResponseModel(
+        val userId: UUID,
+        val username: String,
+        val email: String,
+        val affiliation: String,
+        val country: String
+    )
+}
