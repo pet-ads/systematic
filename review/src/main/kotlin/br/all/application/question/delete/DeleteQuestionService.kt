08b18bec8aec38986e1cd5f0a3b44c07d8f03840
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-17 23:58:28 -0300

    feat(delete-question): add DeleteQuestion service and presenter interface

---
+++ review/src/main/kotlin/br/all/application/question/delete/DeleteQuestionService.kt
@@ -0,0 +1,22 @@
+package br.all.application.question.delete
+
+import io.swagger.v3.oas.annotations.media.Schema
+import java.util.UUID
+
+interface DeleteQuestionService {
+    fun delete(presenter: DeleteQuestionPresenter, request: RequestModel)
+
+    data class RequestModel(
+        val userId: UUID,
+        val systematicStudyId: UUID,
+        val questionId: UUID,
+        val questionContext: String,
+    )
+
+    @Schema(name = "DeleteQuestionResponseModel")
+    data class ResponseModel(
+        val userId: UUID,
+        val systematicStudyId: UUID,
+        val questionId: UUID,
+    )
+}
