c50185a8fc71e3746b278e74361e70c4e6d88e8f
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-21 16:05:46 -0300

    feat(study-review): add service and presenter for retrieving included study reviews with pagination and sorting

---
+++ review/src/main/kotlin/br/all/application/study/find/service/FindAllIncludedStudyReviewsService.kt
@@ -0,0 +1,32 @@
+package br.all.application.study.find.service
+
+import br.all.application.study.find.presenter.FindAllIncludedStudyReviewsPresenter
+import br.all.application.study.repository.StudyReviewDto
+import io.swagger.v3.oas.annotations.media.Schema
+import java.util.UUID
+
+interface FindAllIncludedStudyReviewsService {
+
+    fun findAllIncluded(presenter: FindAllIncludedStudyReviewsPresenter, request: RequestModel)
+
+    data class RequestModel(
+        val userId: UUID,
+        val systematicStudyId: UUID,
+        val searchSessionId: UUID,
+        val page: Int = 0,
+        val pageSize: Int = 20,
+        val sort: String = "id,asc"
+    )
+
+    @Schema(name = "FindAllIncludedStudyReviewsResponseModel")
+    data class ResponseModel(
+        val userId: UUID,
+        val systematicStudyId: UUID,
+        val searchSessionId: UUID,
+        val studyReviews: List<StudyReviewDto>,
+        val page: Int,
+        val size: Int,
+        val totalElements: Long,
+        val totalPages: Int
+    )
+}
